0e45557e30e68ff081a6faea05e4c312df916556
Paul Smith <paulcsmith0218@gmail.com>
2017-01-27 10:53:07 -0500

    Compile time errors when route inference fails

--- src/lucky_web/routeable.cr
+++ src/lucky_web/routeable.cr
@@ -1,31 +1,50 @@
 module LuckyWeb::Routeable
   macro included
-    LuckyWeb::Router.add(infer_path, {{@type.name.id}})
+    add_route_and_helpers
   end
 
-  macro infer_path
-    resource = {{@type.name.split("::").first.underscore}}
-    action_name = {{@type.name.split("::").last.gsub(/Action/, "").underscore}}
+  macro add_route_and_helpers
+    {% resource = @type.name.split("::").first.underscore %}
+    {% action_name = @type.name.split("::").last.gsub(/Action/, "").underscore %}
 
-    case action_name
-    when "index"
-      "/#{resource}"
-    when "new"
-      "/#{resource}/new"
-    when "show"
-      "/#{resource}/:id"
-    else
-      raise <<-ERROR
-      Could not infer route for #{"{{@type.name}}".colorize(:red)}
+    {% if action_name == "index" %}
+      {% path = "/#{resource.id}" %}
+    {% elsif action_name == "new" %}
+      {% path = "/#{resource.id}/new" %}
+    {% else %}
+      {% raise(
+           <<-ERROR
+        Could not infer route for #{@type.name}
 
-      Got:
-        #{"{{@type.name}}".colorize(:red)} #{"(missing a resourceful action)".colorize(:yellow)}
+        Got:
+          #{@type.name} (missing a known resourceful action)
 
-      Expected something like:
-        ResourceName::#{"Index".colorize.mode(:underline)}Action # Index, Show, New, Create, Edit, Update, or Delete
+        Expected something like:
+          ResourceName::IndexAction # Index, Show, New, Create, Edit, Update, or Delete
+        ERROR
+         ) %}
+    {% end %}
 
-      \n
-      ERROR
-    end
+    LuckyWeb::Router.add({{path}}, {{@type.name.id}})
+    # case action_name
+    # when "index"
+    #   "/#{resource}"
+    # when "new"
+    #   "/#{resource}/new"
+    # when "show"
+    #   "/#{resource}/:id"
+    # else
+    #   raise <<-ERROR
+    #   Could not infer route for #{"{{@type.name}}".colorize(:red)}
+    #
+    #   Got:
+    #     #{"{{@type.name}}".colorize(:red)} #{"(missing a resourceful action)".colorize(:yellow)}
+    #
+    #   Expected something like:
+    #     ResourceName::#{"Index".colorize.mode(:underline)}Action # Index, Show, New, Create, Edit, Update, or Delete
+    #
+    #   \n
+    #   ERROR
+    # end
   end
 end
