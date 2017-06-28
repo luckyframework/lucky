e768d9e895b95d676e3409a1e12ee506b22b9c26
Paul Smith <paulcsmith@users.noreply.github.com>
2017-06-27 22:02:42 -0400

    Call next if no route matches.
    Otherwise static files won't be caught. Instead add a separate NotFoundHandler that goes at the end

--- src/lucky_web/route_handler.cr
+++ src/lucky_web/route_handler.cr
@@ -4,9 +4,9 @@ class LuckyWeb::RouteHandler
   def call(context)
     handler = LuckyWeb::Router.find_action(context.request)
     if handler.found?
-      body = handler.payload.new(context, handler.params).perform_action
+      handler.payload.new(context, handler.params).perform_action
     else
-      context.response.print "Action not found for #{context.request.path}"
+      call_next(context)
     end
   end
 end
