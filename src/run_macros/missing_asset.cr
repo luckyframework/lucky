89ea9732c7cfe6bc4c54a73da9a85e3c703d6cb8
Paul Smith <paulcsmith0218@gmail.com>
2017-08-02 21:54:49 +0200

    Show more helpful asset errors

--- src/run_macros/missing_asset.cr
+++ src/run_macros/missing_asset.cr
@@ -1,9 +1,18 @@
 require "colorize"
 require "json"
+require "levenshtein"
 
 asset = ARGV.first
+manifest_path = File.expand_path("./public/manifest.json")
+manifest_file = File.read(manifest_path)
+manifest = JSON.parse(manifest_file)
+
+finder = Levenshtein::Finder.new asset
+manifest.each do |asset_path, _|
+  finder.test asset_path.as_s
+end
 
 puts "#{asset} does not exist in the manifest.".colorize(:red)
-puts "Make sure webpack is running and the asset exists.".colorize(:red)
+puts %(Did you mean "#{finder.best_match}"?).colorize(:yellow)
 
 raise "There was a problem finding the asset"
