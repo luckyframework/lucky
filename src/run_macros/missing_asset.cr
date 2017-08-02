a60fbce1058029398b9b3f0ddce5c5911f195c67
Paul Smith <paulcsmith0218@gmail.com>
2017-08-02 22:02:28 +0200

    Show did you mean? for assets

--- src/run_macros/missing_asset.cr
+++ src/run_macros/missing_asset.cr
@@ -2,17 +2,19 @@ require "colorize"
 require "json"
 require "levenshtein"
 
-asset = ARGV.first
+missing_asset = ARGV.first
 manifest_path = File.expand_path("./public/manifest.json")
 manifest_file = File.read(manifest_path)
 manifest = JSON.parse(manifest_file)
 
-finder = Levenshtein::Finder.new asset
-manifest.each do |asset_path, _|
-  finder.test asset_path.as_s
-end
+best_match = Levenshtein::Finder.find missing_asset, manifest.map(&.to_s), tolerance: 4
 
-puts "#{asset} does not exist in the manifest.".colorize(:red)
-puts %(Did you mean "#{finder.best_match}"?).colorize(:yellow)
+puts %("#{missing_asset}" does not exist in the manifest).colorize(:red)
+
+if best_match
+  puts %(Did you mean "#{best_match}"?).colorize(:yellow)
+else
+  puts "Make sure the asset exists and you have compiled your assets".colorize(:red)
+end
 
 raise "There was a problem finding the asset"
