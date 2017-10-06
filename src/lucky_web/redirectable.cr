82f31679480ce4d1f50b26382cd5c24d44499156
Paul Smith <paulcsmith0218@gmail.com>
2017-10-06 16:02:37 -0400

    Extract Redirectable module

---
+++ src/lucky_web/redirectable.cr
@@ -0,0 +1,15 @@
+module LuckyWeb::Redirectable
+  def redirect(to route : LuckyWeb::RouteHelper, status = 302)
+    redirect to: route.path, status: status
+  end
+
+  def redirect(to action : LuckyWeb::Action.class, status = 302)
+    redirect to: action.route, status: status
+  end
+
+  def redirect(to path : String, status = 302)
+    context.response.headers.add "Location", path
+    context.response.status_code = status
+    LuckyWeb::Response.new(context, "", "")
+  end
+end
