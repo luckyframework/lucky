1fe5e7f91683d4c507c095737c6d170d8d7dcb70
Paul Smith <paulcsmith0218@gmail.com>
2017-10-06 15:44:01 -0400

    Make sure callbacks return the correct types

--- src/lucky_web/action_callbacks.cr
+++ src/lucky_web/action_callbacks.cr
@@ -1,4 +1,7 @@
 module LuckyWeb::ActionCallbacks
+  class Continue
+  end
+
   macro included
     AFTER_CALLBACKS = [] of Symbol
     BEFORE_CALLBACKS = [] of Symbol
@@ -32,6 +35,7 @@ module LuckyWeb::ActionCallbacks
   macro run_before_callbacks
     {% for callback_method in BEFORE_CALLBACKS %}
       callback_result = {{ callback_method }}
+      ensure_callbacks_return_response_or_continue(callback_result)
       if callback_result.is_a?(LuckyWeb::Response)
         return callback_result
       end
@@ -41,9 +45,27 @@ module LuckyWeb::ActionCallbacks
   macro run_after_callbacks
     {% for callback_method in AFTER_CALLBACKS %}
       callback_result = {{ callback_method }}
+
+      ensure_callbacks_return_response_or_continue(callback_result)
+      # Callback {{ callback_method }} should return a LuckyWeb::Response or LuckyWeb::ActionCallbacks::Continue
+      # Do this by using `continue` or one of rendering methods like `render` or `redirect`
+      #
+      #   def {{ callback_method }}
+      #     cookies["name"] = "John"
+      #     continue # or redirect, render
+      #   end
+      #
+
       if callback_result.is_a?(LuckyWeb::Response)
         return callback_result
       end
     {% end %}
+  end
+
+  def ensure_callbacks_return_response_or_continue(callback_result : LuckyWeb::Response | LuckyWeb::ActionCallbacks::Continue)
+  end
+
+  private def continue
+    LuckyWeb::ActionCallbacks::Continue.new
   end
 end
