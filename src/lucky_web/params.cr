897e04b073cc1f41358b25d281271f7b7484c733
Paul Smith <paulcsmith0218@gmail.com>
2017-10-25 20:09:17 -0400

    Rename params -> form_params

--- src/lucky_web/params.cr
+++ src/lucky_web/params.cr
@@ -2,7 +2,6 @@ class LuckyWeb::Params
   include LuckyRecord::Paramable
 
   @request : HTTP::Request
-  @params : HTTP::Params?
   @route_params : Hash(String, String) = {} of String => String
 
   getter :request
@@ -16,7 +15,7 @@ class LuckyWeb::Params
   end
 
   def get(key : String | Symbol)
-    route_params[key.to_s]? || params[key.to_s]? || query_params[key.to_s]?
+    route_params[key.to_s]? || form_params[key.to_s]? || query_params[key.to_s]?
   end
 
   def nested!(nested_key)
@@ -30,7 +29,7 @@ class LuckyWeb::Params
 
   def nested(nested_key : String | Symbol)
     nested_key = "#{nested_key}:"
-    params.to_h.reduce(empty_params) do |nested_params, (key, value)|
+    form_params.to_h.reduce(empty_params) do |nested_params, (key, value)|
       if key.starts_with? nested_key
         nested_params[key.gsub(/^#{Regex.escape(nested_key)}/, "")] = value
       end
@@ -39,8 +38,9 @@ class LuckyWeb::Params
     end
   end
 
-  private def params
-    @params ||= HTTP::Params.parse(body)
+  @_params : HTTP::Params?
+  private def form_params
+    @_form_params ||= HTTP::Params.parse(body)
   end
 
   private def body
