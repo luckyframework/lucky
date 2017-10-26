e02c96040be63c4db0f6f56718a745c25d42e295
Paul Smith <paulcsmith0218@gmail.com>
2017-10-25 21:54:52 -0400

    Configure required settings in sample app

--- src/server.cr
+++ src/server.cr
@@ -2,9 +2,19 @@ require "./lucky_web"
 require "./app/**"
 require "colorize"
 
+LuckyWeb::Server.configure do
+  settings.secret_key_base = "super_secret"
+end
+
+LuckyWeb::Session::Store.configure do
+  settings.key = "test_app"
+  settings.secret = "super-secret"
+end
+
 server = HTTP::Server.new("127.0.0.1", 8080, [
   HTTP::ErrorHandler.new,
   HTTP::LogHandler.new,
+  LuckyWeb::Flash::Handler.new,
   LuckyWeb::RouteHandler.new,
 ])
 
