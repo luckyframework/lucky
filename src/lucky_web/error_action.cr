e59344c4b53762537a0110c103b0a0da3dab559a
Paul Smith <paulcsmith0218@gmail.com>
2017-10-28 10:25:59 -0400

    Ensure Response is returned in handler

--- src/lucky_web/error_action.cr
+++ src/lucky_web/error_action.cr
@@ -13,6 +13,26 @@ abstract class LuckyWeb::ErrorAction
   abstract def handle_error(error : Exception) : LuckyWeb::Response
 
   def perform_action(error : Exception)
-    handle_error(error).print
+    response = handle_error(error)
+    ensure_response_is_returned(response)
+    response.print
+  end
+
+  private def ensure_response_is_returned(response : LuckyWeb::Response) : LuckyWeb::Response
+    response
+  end
+
+  private def ensure_response_is_returned(response)
+    {% raise <<-ERROR
+      You must return a LuckyWeb::Response from handle_error. You can do that by using
+      head, render, redirect, json, render_text, etc.
+
+      Example:
+        def handle_error(error : Exception)
+          # Returns a LuckyWeb::Response
+          # Could also be render, json, render_text, etc.
+          head status: 500
+        end
+      ERROR %}
   end
 end
