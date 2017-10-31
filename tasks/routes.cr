5864f8e18476d6e278f5a0713b3e49e00c21bf05
Michael Kohl <citizen428@gmail.com>
2017-10-31 23:58:11 +0700

    Use shell-table.cr for 'lucky routes' (#175)

--- tasks/routes.cr
+++ tasks/routes.cr
@@ -1,11 +1,20 @@
 require "colorize"
+require "shell-table"
 
 class Routes < LuckyCli::Task
   banner "Show all the routes for the app"
 
   def call
-    LuckyWeb::Router.routes.each do |route|
-      puts "#{route.method.to_s.upcase} #{route.path.colorize(:green)} #{route.action}"
+    routes = LuckyWeb::Router.routes.map do |route|
+      [route.method.to_s.upcase, route.path.colorize(:green), route.action]
     end
+
+    table = ShellTable.new(
+      labels: ["Verb", "URI", "Action"],
+      label_color: :blue,
+      rows: routes
+    )
+
+    puts table
   end
 end
