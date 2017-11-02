5f806ddc8cfebbc0fbd2fd8d9d6497e07981ae64
Alireza Bashiri <azbshiri@gmail.com>
2017-11-03 01:26:43 +0330

    Update spec

--- spec/lucky_web/select_helpers_spec.cr
+++ spec/lucky_web/select_helpers_spec.cr
@@ -11,7 +11,7 @@ class SomeFormWithCompany
   def company_id
     field = LuckyRecord::Field(String).new(
       name: :company_id,
-      param: "company_id",
+      param: "1",
       value: "",
       form_name: "company"
     )
@@ -21,18 +21,20 @@ end
 
 describe LuckyWeb::SelectHelpers do
   it "renders select" do
-    (view.select form.company_id do; end).to_s.should contain <<-HTML
-      <select name="company:company_id"></select>
+    (view.select_input form.company_id do; end).to_s.should contain <<-HTML
+    <select name="company:company_id"></select>
     HTML
   end
 
-  it "renders select with options" do
-    view.select form.company_id do
-      options_for_select(form.company_id, [{"Volvo", 1}])
-    end.to_s.should contain <<-HTML
-      <select name="company:company_id">
-        <option value="1">Volvo</option>
-      </select>
+  it "renders option" do
+    (view.options_for_select form.company_id, [{"Volvo", 2}]).to_s.should contain <<-HTML
+    <option value="2" selected="false">Volvo</option>
+    HTML
+  end
+
+  it "renders selected option" do
+    (view.options_for_select form.company_id, [{"Volvo", 1}]).to_s.should contain <<-HTML
+    <option value="1" selected="true">Volvo</option>
     HTML
   end
 end
