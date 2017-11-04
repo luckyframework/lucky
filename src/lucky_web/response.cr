85bc7907a30b2a3851bad833b6b474afe12446b2
Paul Smith <paulcsmith0218@gmail.com>
2017-11-04 18:36:35 -0400

    Fix redirect and response status
    Close #187
    
    The problem was that you were forced to pass a status to response, so if
    you set the status code on the context and then printed a
    `LuckyWeb::Response` the original status would be overwritten.
    
    This changes it so that the status can be `nil` on `LuckyWeb::Response`
    and it defaults to the one already set on the context, or lastly
    defaults to 200.

--- src/lucky_web/response.cr
+++ src/lucky_web/response.cr
@@ -1,12 +1,18 @@
 class LuckyWeb::Response
-  getter :context, :content_type, :body, :status
+  DEFAULT_STATUS = 200
 
-  def initialize(@context : HTTP::Server::Context, @content_type : String, @body : String, @status : Int32 = 200)
+  getter context, content_type, body
+
+  def initialize(@context : HTTP::Server::Context, @content_type : String, @body : String, @status : Int32? = nil)
   end
 
   def print
     context.response.content_type = content_type
     context.response.status_code = status
     context.response.print body
+  end
+
+  def status
+    @status || context.response.status_code || DEFAULT_STATUS
   end
 end
