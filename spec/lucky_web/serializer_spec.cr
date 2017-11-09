f986af1c28e9960af4d92ffb4a9083b9f51c5841
Paul Smith <paulcsmith0218@gmail.com>
2017-11-09 10:12:24 -0500

    Handle nested serializers

--- spec/lucky_web/serializer_spec.cr
+++ spec/lucky_web/serializer_spec.cr
@@ -9,12 +9,24 @@ private class TestJSON < LuckyWeb::Serializer
   end
 end
 
+private class TestNestedJSON < LuckyWeb::Serializer
+  def initialize(@user_name : String)
+  end
+
+  def render
+    {user: TestJSON.new(@user_name)}
+  end
+end
+
 describe LuckyWeb::Serializer do
   describe "#to_json" do
     it "calls to_json on the render data" do
-      user = TestJSON.new(user_name: "Rey")
+      TestJSON.new(user_name: "Rey").to_json.should eq({name: "Rey"}.to_json)
+    end
 
-      user.to_json.should eq({name: "Rey"}.to_json)
+    it "handles nested JSON classes" do
+      nested = TestNestedJSON.new(user_name: "Picard")
+      nested.to_json.should eq({user: {name: "Picard"}}.to_json)
     end
   end
 end
