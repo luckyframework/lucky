d86b12569ff99500f0320faca27caaf19e2dd277
Uģis Ozols <ugis.ozolss@gmail.com>
2017-11-15 23:21:29 +0200

    Fix precision option implementation.

--- src/lucky_web/page_helpers.cr
+++ src/lucky_web/page_helpers.cr
@@ -1,25 +1,23 @@
 module LuckyWeb::PageHelpers
+  DEFAULT_PRECISION       = 2
   DEFAULT_UNIT            = "$"
   DEFAULT_SEPARATOR       = "."
   DEFAULT_DELIMITER       = ","
   DEFAULT_DELIMITER_REGEX = /(\d)(?=(\d\d\d)+(?!\d))/
 
   def number_to_currency(value : Float, **options)
-    precision = options[:precision]?
-    if precision
-      value = value.round(precision)
-    end
-
     number_to_currency(value.to_s, **options)
   end
 
   def number_to_currency(value : String, **options)
+    precision = options.fetch(:precision, DEFAULT_PRECISION)
     unit = options.fetch(:unit, DEFAULT_UNIT)
     separator = options.fetch(:separator, DEFAULT_SEPARATOR)
     delimiter = options.fetch(:delimiter, DEFAULT_DELIMITER)
 
-    left, right = value.split(".")
+    value = "%.#{precision}f" % value
 
+    left, right = value.split(".")
     left = left.gsub(DEFAULT_DELIMITER_REGEX) do |digit_to_delimit|
       "#{digit_to_delimit}#{delimiter}"
     end
