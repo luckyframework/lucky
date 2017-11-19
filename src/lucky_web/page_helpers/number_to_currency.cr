2b745e31826eb57fdf504a4d337f813e8788c528
Uģis Ozols <ugis.ozolss@gmail.com>
2017-11-20 01:26:02 +0200

    Add number_to_currency (#234)
    * WIP version of number_to_currency helper.
    
    * Fix precision option implementation.
    
    * Accept Int32 in number_to_currency.
    
    * Specify types for each param and accept delimiter_pattern option.
    
    * Number to currency accept nil as an argument.
    
    * Accept format option for number_to_currency.
    
    * Accept negative_format option for number_to_currency.
    
    * Move number_to_currency helper to it's own module.
    
    * Don't accept nil in number_to_currency.
    
    * Convert all accepted values passed to number_to_currency to strings.
    
    This allows to have only one method instead of two.
    
    * Put each accepted argument on a new line.

---
+++ src/lucky_web/page_helpers/number_to_currency.cr
@@ -0,0 +1,35 @@
+module LuckyWeb::NumberToCurrency
+  DEFAULT_PRECISION       = 2
+  DEFAULT_UNIT            = "$"
+  DEFAULT_SEPARATOR       = "."
+  DEFAULT_DELIMITER       = ","
+  DEFAULT_DELIMITER_REGEX = /(\d)(?=(\d\d\d)+(?!\d))/
+  DEFAULT_FORMAT          = "%u%n"
+
+  def number_to_currency(value : Float | Int32 | String,
+                         precision : Int32 = DEFAULT_PRECISION,
+                         unit : String = DEFAULT_UNIT,
+                         separator : String = DEFAULT_SEPARATOR,
+                         delimiter : String = DEFAULT_DELIMITER,
+                         delimiter_pattern : Regex = DEFAULT_DELIMITER_REGEX,
+                         format : String = DEFAULT_FORMAT,
+                         negative_format : String = DEFAULT_FORMAT)
+    value = value.to_s
+
+    if value.to_f.sign == -1
+      format = negative_format if negative_format != DEFAULT_FORMAT
+      value = value.to_f.abs.to_s
+    end
+
+    value = "%.#{precision}f" % value
+
+    left, right = value.split(".")
+    left = left.gsub(delimiter_pattern) do |digit_to_delimit|
+      "#{digit_to_delimit}#{delimiter}"
+    end
+
+    number = "#{left}#{separator}#{right}"
+
+    format.gsub("%n", number).gsub("%u", unit)
+  end
+end
