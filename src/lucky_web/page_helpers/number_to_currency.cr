c674edc94fc28ca4ed93081326ab8d3ae6e1575c
Uģis Ozols <ugis.ozolss@gmail.com>
2017-11-19 23:58:32 +0200

    Put each accepted argument on a new line.

--- src/lucky_web/page_helpers/number_to_currency.cr
+++ src/lucky_web/page_helpers/number_to_currency.cr
@@ -6,7 +6,14 @@ module LuckyWeb::NumberToCurrency
   DEFAULT_DELIMITER_REGEX = /(\d)(?=(\d\d\d)+(?!\d))/
   DEFAULT_FORMAT          = "%u%n"
 
-  def number_to_currency(value : Float | Int32 | String, precision : Int32 = DEFAULT_PRECISION, unit : String = DEFAULT_UNIT, separator : String = DEFAULT_SEPARATOR, delimiter : String = DEFAULT_DELIMITER, delimiter_pattern : Regex = DEFAULT_DELIMITER_REGEX, format : String = DEFAULT_FORMAT, negative_format : String = DEFAULT_FORMAT)
+  def number_to_currency(value : Float | Int32 | String,
+                         precision : Int32 = DEFAULT_PRECISION,
+                         unit : String = DEFAULT_UNIT,
+                         separator : String = DEFAULT_SEPARATOR,
+                         delimiter : String = DEFAULT_DELIMITER,
+                         delimiter_pattern : Regex = DEFAULT_DELIMITER_REGEX,
+                         format : String = DEFAULT_FORMAT,
+                         negative_format : String = DEFAULT_FORMAT)
     value = value.to_s
 
     if value.to_f.sign == -1
