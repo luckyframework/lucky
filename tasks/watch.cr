e7e710bc7ddb81b6e3259b5740b1a890747fb707
mikeeus <itsmikias@gmail.com>
2017-11-23 20:58:32 +0300

    Do not run app process in a subshell

--- tasks/watch.cr
+++ tasks/watch.cr
@@ -27,7 +27,7 @@ module Sentry
     private def create_app_processes
       @app_processes.clear
       result = @run_commands.each do |command|
-        @app_processes << Process.new(command, shell: true, output: true, error: true)
+        @app_processes << Process.new(command, shell: false, output: true, error: true)
       end
 
       @successful_compilations += 1
