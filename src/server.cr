6fb73f83a01231378144036fc9b586eb0c852c90
Paul Smith <paulcsmith0218@gmail.com>
2017-11-30 18:02:24 -0500

    Add new config to test app

--- src/server.cr
+++ src/server.cr
@@ -18,6 +18,29 @@ server = HTTP::Server.new("127.0.0.1", 8080, [
   Lucky::RouteHandler.new,
 ])
 
+LuckyRecord::Repo.configure do
+  settings.url = ""
+end
+
+Lucky::StaticFileHandler.configure do
+  settings.hide_from_logs = true
+end
+
+Lucky::ErrorHandler.configure do
+  settings.show_debug_output = true
+end
+
+Lucky::LogHandler.configure do
+  settings.show_timestamps = false
+end
+
+Lucky::Server.configure do
+  settings.host = "0.0.0.0"
+  settings.port = 8080
+end
+
+Habitat.raise_if_missing_settings!
+
 puts "Listening on http://127.0.0.1:8080...".colorize(:green)
 
 # TODO: Make sure finishe macro works for rendering
