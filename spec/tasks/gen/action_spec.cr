4498082bf98607a0e64b4863a8a0400db5c4110c
Ian Agne <iagne@tscadvantage.com>
2017-12-01 11:03:14 -0600

    Add `lucky gen.action` to generate a basic action
    Add a task to generate an action given the name of the action (e.g.
    Users::Index).  Of note, this task generates the class skeleton but does
    not generate methods within the class.
    
    See issue #83

---
+++ spec/tasks/gen/action_spec.cr
@@ -0,0 +1,37 @@
+require "../../spec_helper"
+
+describe Gen::Action do
+  it "generates a basic action" do
+    begin
+      ARGV.push("Users::Index")
+      Gen::Action.new.call
+
+      File.exists?("src/actions/users/index.cr").should be_true
+    ensure
+      ARGV.pop
+      FileUtils.rm_rf "src/actions/users/index.cr"
+    end
+  end
+
+  it "generates a nested action" do
+    begin
+      ARGV.push("Users::Announcements::Index")
+      Gen::Action.new.call
+
+      File.exists?("src/actions/users/announcements/index.cr").should be_true
+    ensure
+      ARGV.pop
+      FileUtils.rm_rf "src/actions/users/announcements/index.cr"
+    end
+  end
+
+  it "displays an error if given no arguments" do
+    io = IO::Memory.new
+
+    Gen::Action.new.call(io)
+    message = "\e[31mAction name is required. Example: lucky gen.action Users::Index\e[0m"
+
+    io.to_s.strip.should eq(message)
+  end
+end
+
---
+++ tasks/gen/action.cr
@@ -0,0 +1,54 @@
+require "lucky_cli"
+require "colorize"
+require "ecr"
+require "file_utils"
+
+class Lucky::ActionGenerator
+  getter :name
+
+  ECR.def_to_s "#{__DIR__}/action.ecr"
+
+  def initialize(@name : String)
+  end
+
+  def generate
+    make_folders_if_missing
+    File.write(filename, contents)
+  end
+
+  private def make_folders_if_missing
+    FileUtils.mkdir_p Dir.current + "/src/actions/#{path}"
+  end
+
+  private def path_args
+    name.split("::").map(&.downcase)
+  end
+
+  private def path
+    path_args[0..-2].join("/")
+  end
+
+  private def action
+    path_args.last
+  end
+
+  private def filename
+    Dir.current + "/src/actions/#{path}/#{action}.cr"
+  end
+
+  private def contents
+    to_s
+  end
+end
+
+class Gen::Action < LuckyCli::Task
+  banner "Generate a new action"
+
+  def call(io : IO = STDOUT)
+    if ARGV.first? == nil
+      io.puts "Action name is required. Example: lucky gen.action Users::Index".colorize(:red)
+    else
+      Lucky::ActionGenerator.new(name: ARGV.first).generate
+    end
+  end
+end
