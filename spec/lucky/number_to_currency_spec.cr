9a8ac8cc446f9274516e16672bf499a7b20c4cc3
Paul Smith <paulcsmith0218@gmail.com>
2017-12-03 20:25:24 -0500

    Output currency directly to page
    Closes #250

--- spec/lucky/number_to_currency_spec.cr
+++ spec/lucky/number_to_currency_spec.cr
@@ -1,4 +1,5 @@
 require "../../spec_helper"
+require "html"
 
 private class TestPage
   include Lucky::HTMLPage
@@ -10,44 +11,44 @@ end
 describe Lucky::NumberToCurrency do
   describe "number_to_currency" do
     it "accepts Float" do
-      view.number_to_currency(29.92).should eq "$29.92"
+      view.number_to_currency(29.92).to_s.should eq(HTML.escape("$29.92"))
     end
 
     it "accepts String" do
-      view.number_to_currency("92.29").should eq "$92.29"
+      view.number_to_currency("92.29").to_s.should eq(HTML.escape("$92.29"))
     end
 
     it "accepts Integer" do
-      view.number_to_currency(92).should eq "$92.00"
+      view.number_to_currency(92).to_s.should eq(HTML.escape("$92.00"))
     end
 
     describe "options" do
       it "accepts precision" do
-        view.number_to_currency(29.929123, precision: 3).should eq "$29.929"
+        view.number_to_currency(29.929123, precision: 3).to_s.should eq(HTML.escape("$29.929"))
       end
 
       it "accepts unit" do
-        view.number_to_currency(29.92, unit: "CAD").should eq "CAD29.92"
+        view.number_to_currency(29.92, unit: "CAD").to_s.should eq "CAD29.92"
       end
 
       it "accepts separator" do
-        view.number_to_currency(29.92, separator: "-").should eq "$29-92"
+        view.number_to_currency(29.92, separator: "-").to_s.should eq(HTML.escape("$29-92"))
       end
 
       it "accepts delimiter" do
-        view.number_to_currency(1234567890.29, delimiter: "-").should eq "$1-234-567-890.29"
+        view.number_to_currency(1234567890.29, delimiter: "-").to_s.should eq(HTML.escape("$1-234-567-890.29"))
       end
 
       it "accepts delimiter pattern" do
-        view.number_to_currency(1230000, delimiter_pattern: /(\d+?)(?=(\d\d)+(\d)(?!\d))/).should eq "$12,30,000.00"
+        view.number_to_currency(1230000, delimiter_pattern: /(\d+?)(?=(\d\d)+(\d)(?!\d))/).to_s.should eq(HTML.escape("$12,30,000.00"))
       end
 
       it "accepts format" do
-        view.number_to_currency("1234567890.50", format: "<b>%n</b> %u").should eq "<b>1,234,567,890.50</b> $"
+        view.number_to_currency("1234567890.50", format: "%n ** %u").to_s.should eq(HTML.escape("1,234,567,890.50 ** $"))
       end
 
       it "accepts negative format" do
-        view.number_to_currency("-1234567890.50", negative_format: "<b>%n</b> - %u").should eq "<b>1,234,567,890.50</b> - $"
+        view.number_to_currency("-1234567890.50", negative_format: "%n ** - %u").to_s.should eq(HTML.escape("1,234,567,890.50 ** - $"))
       end
     end
   end
