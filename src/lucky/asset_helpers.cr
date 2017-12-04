cb807535278c5eb062429d078e98b2288b3adac6
Paul Smith <paulcsmith@users.noreply.github.com>
2017-12-03 20:46:29 -0500

    Add dynamic_asset helper (#272)
    Closes #269

--- src/lucky/asset_helpers.cr
+++ src/lucky/asset_helpers.cr
@@ -10,4 +10,13 @@ module Lucky::AssetHelpers
       {{ run "../run_macros/missing_asset", path }}
     {% end %}
   end
+
+  def dynamic_asset(path)
+    fingerprinted_path = ASSET_MANIFEST[path]?
+    if fingerprinted_path
+      "/" + fingerprinted_path
+    else
+      raise "Missing asset: #{path}"
+    end
+  end
 end
