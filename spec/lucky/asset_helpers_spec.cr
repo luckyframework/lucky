1ad057c27c6fb72ba5717b89955f370899c54a57
Paul Smith <paulcsmith0218@gmail.com>
2017-12-03 20:44:06 -0500

    Add dynamic_asset helper
    Closes #269

--- spec/lucky/asset_helpers_spec.cr
+++ spec/lucky/asset_helpers_spec.cr
@@ -3,17 +3,40 @@ require "../spec_helper"
 private class TestPage
   include Lucky::AssetHelpers
 
-  def asset_url
+  def asset_path
     asset("images/logo.png")
+  end
+
+  def dynamic_asset_path
+    interpolated = "logo"
+    dynamic_asset("images/#{interpolated}.png")
+  end
+
+  def missing_dynamic_asset_path
+    dynamic_asset("woops!.png")
   end
 end
 
 describe Lucky::AssetHelpers do
-  it "returns the fingerprinted path" do
-    Lucky::AssetHelpers.asset("images/logo.png").should eq "/images/logo-with-hash.png"
+  describe "compile time asset helper" do
+    it "returns the fingerprinted path" do
+      Lucky::AssetHelpers.asset("images/logo.png").should eq "/images/logo-with-hash.png"
+    end
+
+    it "works when included in another class" do
+      TestPage.new.asset_path.should eq "/images/logo-with-hash.png"
+    end
   end
 
-  it "works when included in another class" do
-    TestPage.new.asset_url.should eq "/images/logo-with-hash.png"
+  describe "dynamic asset helper" do
+    it "returns the fingerprinted path" do
+      TestPage.new.dynamic_asset_path.should eq "/images/logo-with-hash.png"
+    end
+
+    it "raises a helpful error" do
+      expect_raises Exception, "Missing asset: woops!.png" do
+        TestPage.new.missing_dynamic_asset_path
+      end
+    end
   end
 end
