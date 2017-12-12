ea143572e002022c2a354b54fbdacb6e3efba9d8
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-12-12 20:18:29 +0100

    Add getters for exception instance vars

--- src/lucky/exceptions.cr
+++ src/lucky/exceptions.cr
@@ -14,20 +14,24 @@ module Lucky
     end
 
     class InvalidParam < Base
+      getter :param_name, :param_value, :param_type
+
       def initialize(@param_name : String, @param_value : String, @param_type : String)
       end
 
       def message : String?
-        "Non-optional param \"#{@param_name}\" with value \"#{@param_value}\" couldn't be parsed to a \"#{@param_type}\""
+        "Non-optional param \"#{param_name}\" with value \"#{param_value}\" couldn't be parsed to a \"#{param_type}\""
       end
     end
 
     class MissingParam < Base
+      getter :param_name
+
       def initialize(@param_name : String)
       end
 
       def message : String?
-        "Missing parameter: #{@param_name}"
+        "Missing parameter: #{param_name}"
       end
     end
   end
