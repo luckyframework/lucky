0a69bd10e78e57674f5c2ceae758421c8d6a3201
Paul Smith <paulcsmith@users.noreply.github.com>
2017-12-13 21:54:05 -0500

    Allow custom label text in label_for (#296)
    Closes #283

--- spec/lucky/label_helpers_spec.cr
+++ spec/lucky/label_helpers_spec.cr
@@ -32,6 +32,14 @@ private class TestPage
     label_for form.first_name, class: "best-label"
   end
 
+  def label_with_custom_text
+    label_for form.first_name, "My Label"
+  end
+
+  def label_with_custom_text_and_options
+    label_for form.first_name, "My Label", class: "best-label"
+  end
+
   private def form
     TestForm.new
   end
@@ -45,6 +53,14 @@ describe Lucky::LabelHelpers do
 
     view.label_with_html_options.to_s.should contain <<-HTML
     <label for="user_first_name" class="best-label">First name</label>
+    HTML
+
+    view.label_with_custom_text.to_s.should contain <<-HTML
+    <label for="user_first_name">My Label</label>
+    HTML
+
+    view.label_with_custom_text_and_options.to_s.should contain <<-HTML
+    <label for="user_first_name" class="best-label">My Label</label>
     HTML
   end
 end
--- src/lucky/tags/label_helpers.cr
+++ src/lucky/tags/label_helpers.cr
@@ -1,7 +1,15 @@
 module Lucky::LabelHelpers
   def label_for(field : LuckyRecord::AllowedField, **html_options)
-    label(
+    label_for(
+      field,
       LuckyInflector::Inflector.humanize(field.name.to_s),
+      **html_options
+    )
+  end
+
+  def label_for(field : LuckyRecord::AllowedField, text : String, **html_options)
+    label(
+      text,
       merge_options(html_options, {"for" => input_id(field)})
     )
   end
