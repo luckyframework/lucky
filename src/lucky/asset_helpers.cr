bccde2d7d1d1c1723f6aa5aa9da6a582c43a6d7d
Paul Smith <paulcsmith@users.noreply.github.com>
2017-12-15 15:33:30 -0500

    Raise helpful message if using `asset` incorrectly (#307)
    Closes #274

--- src/lucky/asset_helpers.cr
+++ src/lucky/asset_helpers.cr
@@ -4,10 +4,33 @@ module Lucky::AssetHelpers
   {{ run "../run_macros/generate_asset_helpers" }}
 
   macro asset(path)
-    {% if Lucky::AssetHelpers::ASSET_MANIFEST[path] %}
-      {{ "/" + Lucky::AssetHelpers::ASSET_MANIFEST[path] }}
+    {% if path.is_a?(StringLiteral) %}
+      {% if Lucky::AssetHelpers::ASSET_MANIFEST[path] %}
+        {{ "/" + Lucky::AssetHelpers::ASSET_MANIFEST[path] }}
+      {% else %}
+        {{ run "../run_macros/missing_asset", path }}
+      {% end %}
+    {% elsif path.is_a?(StringInterpolation) %}
+      {% raise <<-ERROR
+      \n
+      The 'asset' macro doesn't work with string interpolation
+
+      Try this...
+
+        ▸ Use the 'dynamic_asset' method instead
+
+      ERROR %}
     {% else %}
-      {{ run "../run_macros/missing_asset", path }}
+      {% raise <<-ERROR
+      \n
+      The 'asset' macro requires a literal string like "my-logo.png", instead got: #{path}
+
+      Try this...
+
+        ▸ If you're using a variable, switch to a literal string
+        ▸ If you can't use a literal string, use the 'dynamic_asset' method instead
+
+      ERROR %}
     {% end %}
   end
 
