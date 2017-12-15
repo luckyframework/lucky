8b0926f5dfb7508f0904c2b9cafb91b4b1758c9e
Paul Smith <paulcsmith0218@gmail.com>
2017-12-15 14:50:15 -0500

    Allow using asset helpers in included modules
    Fixes #293

--- src/lucky/asset_helpers.cr
+++ src/lucky/asset_helpers.cr
@@ -4,15 +4,15 @@ module Lucky::AssetHelpers
   {{ run "../run_macros/generate_asset_helpers" }}
 
   macro asset(path)
-    {% if ASSET_MANIFEST[path] %}
-      {{ "/" + ASSET_MANIFEST[path] }}
+    {% if Lucky::AssetHelpers::ASSET_MANIFEST[path] %}
+      {{ "/" + Lucky::AssetHelpers::ASSET_MANIFEST[path] }}
     {% else %}
       {{ run "../run_macros/missing_asset", path }}
     {% end %}
   end
 
   def dynamic_asset(path)
-    fingerprinted_path = ASSET_MANIFEST[path]?
+    fingerprinted_path = Lucky::AssetHelpers::ASSET_MANIFEST[path]?
     if fingerprinted_path
       "/" + fingerprinted_path
     else
