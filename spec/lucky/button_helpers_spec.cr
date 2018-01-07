3f8b44b8b79230fca8185d6fdebc17489c8e95e3
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-01-07 22:02:42 +0100

    Rework 'button' helper. Buttons are of only three possible types.
    The previous implementation used buttons as links, which is not what
    they are intended for. They don't have a href attribute.

--- spec/lucky/button_helpers_spec.cr
+++ spec/lucky/button_helpers_spec.cr
@@ -1,84 +1,55 @@
 require "../../spec_helper"
 
-class ButtonHelpers::Index < Lucky::Action
-  action { text "foo" }
-end
-
-class ButtonHelpers::Create < Lucky::Action
-  action { text "foo" }
-end
-
 private class TestPage
   include Lucky::HTMLPage
 
   def render
   end
 
-  def get_route
-    button "Test", to: ButtonHelpers::Index
+  def submit_button
+    button "Submit me!"
   end
 
-  def non_get_route
-    button "Test", to: ButtonHelpers::Create
+  def reset_button
+    button "Reset me!", type: "reset"
   end
 
-  def non_get_route_with_options
-    button "Test", to: ButtonHelpers::Create, something_custom: "foo"
+  def disabled_button
+    button "Hey there", disabled: "disabled"
   end
 
-  def string_path
-    button "Test", to: "/foos"
-  end
-
-  def string_path_with_options
-    button "Test", to: "/foos", data_method: "post"
-  end
-
-  def get_route_with_block
-    button to: ButtonHelpers::Index do
+  def submit_button_with_block
+    button do
       text "Hello"
     end
   end
 
-  def string_path_with_block
-    button to: "/foo" do
-      text "Hello"
-    end
+  def raises_for_wrong_type
+    button "Wrong", type: "fail"
   end
 end
 
 describe Lucky::ButtonHelpers do
-  it "renders a button tag" do
-    view.get_route.to_s.should contain %(<button href="/button_helpers">Test</button>)
-    view.non_get_route.to_s.should contain %(<button href="/button_helpers" data-method="post">Test</button>)
-    view
-      .non_get_route_with_options
-      .to_s
-      .should contain %(<button href="/button_helpers" data-method="post" something-custom="foo">Test</button>)
-    view.string_path.to_s.should contain %(<button href="/foos">Test</button>)
-    view.string_path_with_options.to_s.should contain %(<button href="/foos" data-method="post">Test</button>)
+  it "renders a submit button" do
+    view.submit_button.to_s.should contain %(<button type="submit">Submit me!</button>)
   end
 
-  it "renders a link tag with an action" do
-    view.link("Test", to: ButtonHelpers::Index).to_s.should contain <<-HTML
-    <a href="/button_helpers">Test</a>
-    HTML
-
-    link = view.link(to: ButtonHelpers::Index, class: "link") { }
-
-    link.to_s.should contain <<-HTML
-    <a href="/button_helpers" class="link"></a>
-    HTML
+  it "renders a reset button" do
+    view.reset_button.to_s.should contain %(<button type="reset">Reset me!</button>)
   end
 
-  it "renders a button tag with a block" do
-    view.string_path_with_block.to_s.should contain <<-HTML
-    <button href="/foo">Hello</button>
-    HTML
+  it "renders any attributes" do
+    view.disabled_button.to_s.should contain %(<button type="submit" disabled="disabled">Hey there</button>)
+  end
 
-    view.get_route_with_block.to_s.should contain <<-HTML
-    <button href="/button_helpers">Hello</button>
-    HTML
+  it "renders any block content" do
+    view.submit_button_with_block.to_s.should contain %(<button type="submit">Hello</button>)
+  end
+
+  it "raises for invalid types" do
+    expect_raises Exception, "submit, reset or button" do
+      view.raises_for_wrong_type
+    end
   end
 end
 
