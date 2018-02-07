452a1424edccaafc65985b49b3d76434b86680db
Paul Smith <paulcsmith@users.noreply.github.com>
2018-02-06 19:12:31 -0500

    Add render_if_defined (#385)
    Closes #366
    
    This makes it a little easier to optionally render HTML within layouts.

---
+++ spec/lucky/render_if_defined_spec.cr
@@ -0,0 +1,30 @@
+require "../../spec_helper"
+
+include ContextHelper
+
+abstract class LayoutWithOptionalSidebar
+  include Lucky::HTMLPage
+
+  def render
+    render_if_defined :sidebar
+  end
+end
+
+private class PageWithSidebar < LayoutWithOptionalSidebar
+  def sidebar
+    text "In the sidebar"
+  end
+end
+
+private class PageWithoutSidebar < LayoutWithOptionalSidebar
+end
+
+describe "render_if_defined" do
+  it "renders if the method is defined, otherwise it does nothing" do
+    page_with_sidebar = PageWithSidebar.new(build_context)
+    page_without_sidebar = PageWithoutSidebar.new(build_context)
+
+    page_with_sidebar.render.to_s.should eq "In the sidebar"
+    page_without_sidebar.render.to_s.should eq ""
+  end
+end
