e3eeceb135b8a0bfee0ab1173de729df3746708d
Edward Loveall <edward@edwardloveall.com>
2018-03-02 14:17:14 -0500

    Add documentation for Lucky::ContentTypeHelpers

--- src/lucky/content_type_helpers.cr
+++ src/lucky/content_type_helpers.cr
@@ -1,19 +1,35 @@
+# These helpers check HTTP headers to determine "content type". Most check the
+# Content-Type header, check other information, such as the `X-Requested-With`
+# header.
 module Lucky::ContentTypeHelpers
   abstract def content_type : String
   abstract def headers : HTTP::Headers
 
+  # Check if the request is JSON
+  #
+  # This tests if the Content-Type header is `application/json`
   def json?
     content_type == "application/json"
   end
 
+  # Check if the request is AJAX
+  #
+  # This tests if the X-Requested-With header is `XMLHttpRequest`
   def ajax?
     headers["X-Requested-With"]? == "XMLHttpRequest"
   end
 
+  # Check if the request is HTML
+  #
+  # This tests if the Content-Type header is `text/html`
   def html?
     content_type == "text/html"
   end
 
+  # Check if the request is XML
+  #
+  # This tests if the Content-Type header is `application/xml` or
+  # `application/xhtml+xml`
   def xml?
     ["application/xml", "application/xhtml+xml"].includes? content_type
   end
