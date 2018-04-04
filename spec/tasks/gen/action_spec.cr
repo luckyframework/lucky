82d7e02cf37c5ab0994de7db633b1f987d0752e1
mikeeus <itsmikias@gmail.com>
2018-04-04 23:48:44 +0300

    Refactor generate method

--- spec/tasks/gen/action_spec.cr
+++ spec/tasks/gen/action_spec.cr
@@ -5,11 +5,8 @@ include CleanupHelper
 describe Gen::Action do
   it "generates a basic browser action" do
     with_cleanup do
-      io = IO::Memory.new
       valid_action_name = "Users::Index"
-      ARGV.push(valid_action_name)
-
-      Gen::Action::Browser.new.call(io)
+      io = generate valid_action_name, Gen::Action::Browser
 
       index_action = File.read("./src/actions/users/index.cr")
       index_action.should contain(valid_action_name)
@@ -22,11 +19,8 @@ describe Gen::Action do
 
   it "generates a basic api action" do
     with_cleanup do
-      io = IO::Memory.new
       valid_action_name = "Users::Index"
-      ARGV.push(valid_action_name)
-
-      Gen::Action::Api.new.call(io)
+      io = generate valid_action_name, Gen::Action::Api
 
       index_action = File.read("./src/actions/users/index.cr")
       index_action.should contain(valid_action_name)
@@ -39,11 +33,8 @@ describe Gen::Action do
 
   it "generates nested browser and api actions" do
     with_cleanup do
-      io = IO::Memory.new
       valid_nested_action_name = "Users::Announcements::Index"
-      ARGV.push(valid_nested_action_name)
-
-      Gen::Action::Browser.new.call(io)
+      io = generate valid_nested_action_name, Gen::Action::Browser
 
       index_action = File.read("src/actions/users/announcements/index.cr")
       index_action.should contain(valid_nested_action_name)
@@ -54,11 +45,8 @@ describe Gen::Action do
     end
 
     with_cleanup do
-      io = IO::Memory.new
       valid_nested_action_name = "Users::Announcements::Index"
-      ARGV.push(valid_nested_action_name)
-
-      Gen::Action::Api.new.call(io)
+      io = generate valid_nested_action_name, Gen::Action::Api
 
       index_action = File.read("src/actions/users/announcements/index.cr")
       index_action.should contain(valid_nested_action_name)
@@ -70,11 +58,8 @@ describe Gen::Action do
 
     it "snake cases filenames of a camel case action" do
       with_cleanup do
-        io = IO::Memory.new
         valid_camel_case_action_name = "Users::HostedEvents"
-        ARGV.push(valid_camel_case_action_name)
-
-        Gen::Action::Browser.new.call(io)
+        io = generate valid_camel_case_action_name, Gen::Action::Browser
 
         File.read("src/actions/users/hosted_events.cr")
             .should contain(valid_camel_case_action_name)
@@ -85,22 +70,23 @@ describe Gen::Action do
   end
 
   it "displays an error if given no arguments" do
-    io = IO::Memory.new
-
-    Gen::Action::Browser.new.call(io)
+    io = generate nil, Gen::Action::Browser
 
     io.to_s.should contain("Action name is required.")
   end
 
   it "displays an error if given only one class" do
     with_cleanup do
-      io = IO::Memory.new
-      invalid_action_name = "Users"
-      ARGV.push(invalid_action_name)
-
-      Gen::Action::Browser.new.call(io)
+      io = generate "Users", Gen::Action::Browser
 
       io.to_s.should contain("That's not a valid Action.")
     end
   end
+end
+
+private def generate(name, generator : Class)
+  ARGV.push(name) if name
+  io = IO::Memory.new
+  generator.new.call(io)
+  io
 end
