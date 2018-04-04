fc3351a7965563e80fba6c939c1a18c7e3ff24a6
mikeeus <itsmikias@gmail.com>
2018-04-04 23:11:22 +0300

    Add tests for api and browser generators

--- spec/tasks/gen/action_spec.cr
+++ spec/tasks/gen/action_spec.cr
@@ -3,31 +3,35 @@ require "../../spec_helper"
 include CleanupHelper
 
 describe Gen::Action do
-  it "generates a basic action" do
+  it "generates a basic browser action" do
     with_cleanup do
       io = IO::Memory.new
       valid_action_name = "Users::Index"
       ARGV.push(valid_action_name)
 
-      Gen::Action.new.call(io)
+      Gen::Action::Browser.new.call(io)
 
-      File.read("./src/actions/users/index.cr")
-          .should contain(valid_action_name)
+      index_action = File.read("./src/actions/users/index.cr")
+      index_action.should contain(valid_action_name)
+      index_action.should contain("< BrowserAction")
+
       io.to_s.should contain(valid_action_name)
       io.to_s.should contain("/src/actions/users")
     end
   end
 
-  it "generates a nested action" do
+  it "generates a nested api action" do
     with_cleanup do
       io = IO::Memory.new
       valid_nested_action_name = "Users::Announcements::Index"
       ARGV.push(valid_nested_action_name)
 
-      Gen::Action.new.call(io)
+      Gen::Action::Api.new.call(io)
 
-      File.read("src/actions/users/announcements/index.cr")
-          .should contain(valid_nested_action_name)
+      index_action = File.read("src/actions/users/announcements/index.cr")
+      index_action.should contain(valid_nested_action_name)
+      index_action.should contain("< ApiAction")
+
       io.to_s.should contain(valid_nested_action_name)
       io.to_s.should contain("/src/actions/users/announcements")
     end
@@ -38,7 +42,7 @@ describe Gen::Action do
         valid_camel_case_action_name = "Users::HostedEvents"
         ARGV.push(valid_camel_case_action_name)
 
-        Gen::Action.new.call(io)
+        Gen::Action::Browser.new.call(io)
 
         File.read("src/actions/users/hosted_events.cr")
             .should contain(valid_camel_case_action_name)
@@ -51,7 +55,7 @@ describe Gen::Action do
   it "displays an error if given no arguments" do
     io = IO::Memory.new
 
-    Gen::Action.new.call(io)
+    Gen::Action::Browser.new.call(io)
 
     io.to_s.should contain("Action name is required.")
   end
@@ -62,7 +66,7 @@ describe Gen::Action do
       invalid_action_name = "Users"
       ARGV.push(invalid_action_name)
 
-      Gen::Action.new.call(io)
+      Gen::Action::Browser.new.call(io)
 
       io.to_s.should contain("That's not a valid Action.")
     end
