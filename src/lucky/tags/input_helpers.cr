80dd5bfadafa92883ba65288cba3534e999c2de6
Paul Smith <paulcsmith0218@gmail.com>
2018-04-24 21:46:23 -0400

    Show helpful error when using disallowed fields

--- src/lucky/tags/input_helpers.cr
+++ src/lucky/tags/input_helpers.cr
@@ -1,7 +1,31 @@
 module Lucky::InputHelpers
+  macro error_message_for_unallowed_field
+    {% raise <<-ERROR
+      The field is not fillable.
+
+      Try this...
+
+        ▸ Allow the field to be filled by adding 'fillable {field_name}' to your form object.
+
+      Q. Why aren't fields fillable by default?
+      A. Malicious users could submit any field they want. For example: you
+         might have an 'admin' flag on a User. If all fields were fillable,
+         a malicious user could set the 'admin' flag to 'true' on any form.
+
+      ERROR %}
+  end
+
+  macro generate_helpful_error_for(input_method_name)
+    def {{ input_method_name.id }}(field, **options)
+      error_message_for_unallowed_field
+    end
+  end
+
   def submit(text : String, **html_options)
     input merge_options(html_options, {"type" => "submit", "value" => text})
   end
+
+  generate_helpful_error_for textarea
 
   def textarea(field : LuckyRecord::FillableField, **html_options)
     textarea field.param.to_s, merge_options(html_options, {
@@ -18,45 +42,67 @@ module Lucky::InputHelpers
     generate_input(field, "checkbox", html_options)
   end
 
+  generate_helpful_error_for checkbox
+
   def text_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "text", html_options)
   end
+
+  generate_helpful_error_for email_input
 
   def email_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "email", html_options)
   end
 
+  generate_helpful_error_for file_input
+
   def file_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "file", html_options)
   end
+
+  generate_helpful_error_for color_input
 
   def color_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "color", html_options)
   end
 
+  generate_helpful_error_for hidden_input
+
   def hidden_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "hidden", html_options)
   end
+
+  generate_helpful_error_for number_input
 
   def number_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "number", html_options)
   end
 
+  generate_helpful_error_for telephone_input
+
   def telephone_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "telephone", html_options)
   end
+
+  generate_helpful_error_for url_input
 
   def url_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "url", html_options)
   end
 
+  generate_helpful_error_for search_input
+
   def search_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "search", html_options)
   end
 
+  generate_helpful_error_for password_input
+
   def password_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "password", html_options, {"value" => ""})
   end
+
+  generate_helpful_error_for range_input
 
   def range_input(field : LuckyRecord::FillableField, **html_options)
     generate_input(field, "range", html_options)
