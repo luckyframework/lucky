3f4cad0349db7168b1c6ffc7ec74ca502c0f8f53
Paul Smith <paulcsmith0218@gmail.com>
2018-04-26 18:38:21 -0400

    Add lucky gen.component

---
+++ tasks/gen/component.cr
@@ -0,0 +1,65 @@
+require "lucky_cli"
+require "teeplate"
+require "colorize"
+require "file_utils"
+
+class Lucky::ComponentTemplate < Teeplate::FileTree
+  @filename : String
+  @class : String
+
+  directory "#{__DIR__}/templates/component"
+
+  def initialize(@filename, @class)
+  end
+end
+
+class Gen::Component < LuckyCli::Task
+  banner "Generate a new component"
+
+  def call(io : IO = STDOUT)
+    if error
+      io.puts error.colorize(:red)
+    else
+      Lucky::ComponentTemplate.new(component_filename, component_class).render(output_path)
+      io.puts success_message
+    end
+  end
+
+  private def error
+    missing_name_error || invalid_format_error
+  end
+
+  private def missing_name_error
+    if ARGV.first?.nil?
+      "Component name is required."
+    end
+  end
+
+  private def invalid_format_error
+    if !component_class.includes?("::")
+      "Components must be namespaced. Example: lucky gen.component Users::Row"
+    end
+  end
+
+  private def component_class
+    ARGV.first
+  end
+
+  private def component_filename
+    component_class.split("::").last.underscore.downcase
+  end
+
+  private def output_path
+    parts = component_class.split("::")
+    parts.pop
+    "./src/components/#{parts.map(&.underscore).map(&.downcase).join("/")}"
+  end
+
+  private def output_path_with_filename
+    output_path + "/" + component_filename + ".cr"
+  end
+
+  private def success_message
+    "Done generating #{component_class.colorize(:green)} in #{output_path_with_filename.colorize(:green)}"
+  end
+end
