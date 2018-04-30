a5d2b4d1b11493f2c2e915f545273f4bb84566ac
Russ Smith <russ@bashme.org>
2018-04-30 11:42:55 -0700

    Passing in the context to the RouteNotFoundError when we hit a 404.
    Adding the context allows the user to configure a handle_error method to
    handle 404's and do whatever they need to do when that happens.

--- src/lucky/route_not_found_handler.cr
+++ src/lucky/route_not_found_handler.cr
@@ -2,6 +2,6 @@ class Lucky::RouteNotFoundHandler
   include HTTP::Handler
 
   def call(context)
-    raise Lucky::RouteNotFoundError.new(context.request.method, context.request.path)
+    raise Lucky::RouteNotFoundError.new(context)
   end
 end
