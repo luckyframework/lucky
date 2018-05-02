273bfd04af2fab87ce362de9624ee264e0ae5d9e
Jeremy Woertink <jeremywoertink@gmail.com>
2018-05-01 17:46:38 -0700

    added in video and source html tags. Added in some specs for that (#447)

--- src/lucky/tags/base_tags.cr
+++ src/lucky/tags/base_tags.cr
@@ -1,7 +1,7 @@
 module Lucky::BaseTags
-  TAGS             = %i(a address article aside b bdi body button code details dialog div em fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header html i label li main mark menuitem meter nav ol option option pre progress rp rt ruby s script section small span strong summary table tbody td textarea th thead time title tr u ul wbr)
+  TAGS             = %i(a address article aside b bdi body button code details dialog div em fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header html i label li main mark menuitem meter nav ol option option pre progress rp rt ruby s script section small span strong summary table tbody td textarea th thead time title tr u ul video wbr)
   RENAMED_TAGS     = {"para": "p", "select_tag": "select"}
-  EMPTY_TAGS       = %i(img br hr input meta)
+  EMPTY_TAGS       = %i(img br hr input meta source)
   EMPTY_HTML_ATTRS = {} of String => String
 
   @view = IO::Memory.new
