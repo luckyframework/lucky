3f63abebbe3f05e097b55dc867aede424369388d
Paul Smith <paulcsmith0218@gmail.com>
2018-05-07 21:49:49 -0400

    More tips for missing assets

--- src/run_macros/missing_asset.cr
+++ src/run_macros/missing_asset.cr
@@ -9,12 +9,13 @@ manifest = JSON.parse(manifest_file)
 
 best_match = Levenshtein::Finder.find missing_asset, manifest.map(&.to_s), tolerance: 4
 
-puts %("#{missing_asset}" does not exist in the manifest).colorize(:red)
+puts %("#{missing_asset}" does not exist in the manifest.).colorize(:red)
 
 if best_match
   puts %(Did you mean "#{best_match}"?).colorize(:yellow)
 else
-  puts "Make sure the asset exists and you have compiled your assets".colorize(:red)
+  puts "Make sure the asset exists and you have compiled your assets.".colorize(:red)
+  puts "If you recently added a static asset or font try stopping the server (ctrl-c) and starting it again (lucky dev).".colorize(:red)
 end
 
 raise "There was a problem finding the asset"
