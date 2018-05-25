a67424cfc008106a5d6c6ca2b30a9c2a801af338
Edward Loveall <edward@edwardloveall.com>
2018-05-25 12:31:53 -0400

    Add test for action callback order
    Callbacks are called in the order they are defined, relative to their
    group. I.e. all the `before` callbacks in defined order, then the
    action, the all the `after` callbacks in their defined order.
    
    This tests ensures that they are always called that way.

--- spec/lucky/action_callbacks_spec.cr
+++ spec/lucky/action_callbacks_spec.cr
@@ -86,6 +86,40 @@ class Callbacks::HaltedAfter < Lucky::Action
   end
 end
 
+class Callbacks::OrderDependent < Lucky::Action
+  getter callback_data
+  @callback_data = [] of String
+
+  before dog
+  before cat
+  after red
+  after yellow
+
+  get "/order_dependent" do
+    text "rendered"
+  end
+
+  def dog
+    @callback_data << "dog"
+    continue
+  end
+
+  def cat
+    @callback_data << "cat"
+    continue
+  end
+
+  def red
+    @callback_data << "red"
+    continue
+  end
+
+  def yellow
+    @callback_data << "yellow"
+    continue
+  end
+end
+
 describe Lucky::Action do
   it "works with actions that use the `action` macro" do
     response = CallbackFromActionMacro::Index.new(build_context, params).call
@@ -119,6 +153,17 @@ describe Lucky::Action do
       response.context.response.status_code.should eq 302
       response.context.response.headers["Location"].should eq "/redirected_in_after"
       response.context.cookies["after"].should be_nil
+    end
+
+    it "renders the callbacks in the order they were defined" do
+      action = Callbacks::OrderDependent.new(build_context, params)
+      response = action.call
+
+      response.body.should eq "rendered"
+      action.callback_data[0].should eq("dog")
+      action.callback_data[1].should eq("cat")
+      action.callback_data[2].should eq("red")
+      action.callback_data[3].should eq("yellow")
     end
   end
 end
