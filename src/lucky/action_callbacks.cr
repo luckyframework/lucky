20d6d1414d1abb6c4c8bcbe2e8b195a805f96ea3
Edward Loveall <edward@edwardloveall.com>
2018-05-25 10:33:37 -0400

    Add callback documentation (after/before)

--- src/lucky/action_callbacks.cr
+++ src/lucky/action_callbacks.cr
@@ -1,7 +1,9 @@
 module Lucky::ActionCallbacks
+  # :nodoc:
   class Continue
   end
 
+  # :nodoc:
   macro included
     AFTER_CALLBACKS = [] of Symbol
     BEFORE_CALLBACKS = [] of Symbol
@@ -14,6 +16,7 @@ module Lucky::ActionCallbacks
     end
   end
 
+  # :nodoc:
   macro inherit_callbacks
     \{% for v in @type.ancestors.first.constant :BEFORE_CALLBACKS %}
       \{% BEFORE_CALLBACKS << v %}
@@ -24,14 +27,64 @@ module Lucky::ActionCallbacks
     \{% end %}
   end
 
+  # Run a method before an action starts
+  #
+  # Methods will run in the order that each `before` is defined. Also, each
+  # method must return a `Lucky::Response` such as `redirect` or `render`, or
+  # call
+  # `continue`:
+  #
+  # ```crystal
+  # class Users::Destroy < BrowserAction
+  #   before check_if_signed_in
+  #   before confirm_destroy
+  #
+  #   delete "/:user_id" do
+  #     # destroy the user :(
+  #   end
+  #
+  #   def check_if_signed_in
+  #     if current_user.nil?
+  #       redirect to: SignInPage
+  #     end
+  #   end
+  #
+  #   def confirm_destroy
+  #     # confirm that the user should be destroyed
+  #     continue
+  #   end
+  # end
+  # ```
   macro before(method_name)
     {% BEFORE_CALLBACKS << method_name.id %}
   end
 
+  # Run a method after an action ends
+  #
+  # `after` isn't as common as `before` but can still be useful. One example
+  # would be to log a successful transaction to analytics. Methods will run in
+  # the order that each `after` is defined. Also, each method must return a
+  # `Lucky::Response` such as `redirect` or `render`, or call `continue`:
+  #
+  # ```crystal
+  # class Purchases::Create < BrowserAction
+  #   after log_transaction
+  #
+  #   action do
+  #     # purchase the product
+  #   end
+  #
+  #   def log_transaction
+  #     # send the purchase to analytics
+  #     continue
+  #   end
+  # end
+  # ```
   macro after(method_name)
     {% AFTER_CALLBACKS << method_name.id %}
   end
 
+  # :nodoc:
   macro run_before_callbacks
     {% for callback_method in BEFORE_CALLBACKS %}
       callback_result = {{ callback_method }}
@@ -50,6 +103,7 @@ module Lucky::ActionCallbacks
     {% end %}
   end
 
+  # :nodoc:
   macro run_after_callbacks
     {% for callback_method in AFTER_CALLBACKS %}
       callback_result = {{ callback_method }}
@@ -69,6 +123,7 @@ module Lucky::ActionCallbacks
     {% end %}
   end
 
+  # :nodoc:
   def ensure_callbacks_return_response_or_continue(callback_result : Lucky::Response | Lucky::ActionCallbacks::Continue)
   end
 
