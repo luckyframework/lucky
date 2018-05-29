63116d3fdc13a3895000408ffed9ad5a2d9f73a3
Paul Smith <paulcsmith0218@gmail.com>
2018-05-28 22:13:33 -0400

    Allow for renderable proc
    This makes components a bit more flexible. This will be used for
    rendering fields.

--- spec/lucky/mount_component_spec.cr
+++ spec/lucky/mount_component_spec.cr
@@ -30,6 +30,30 @@ private class ComplexTestComponent < BaseComponent
   end
 end
 
+private class ComponentWithBlock < BaseComponent
+  alias RenderableProc = Proc(String, Nil)
+  needs name : String
+  needs block : RenderableProc
+
+  def render
+    @block.call(@name)
+  end
+end
+
+private class ComponentWithOptionalBlock < BaseComponent
+  alias RenderableProc = Proc(Nil)
+  needs block : RenderableProc? = nil
+
+  def render
+    block = @block
+    if block
+      block.call
+    else
+      text "No block given"
+    end
+  end
+end
+
 private class TestMountPage
   include Lucky::HTMLPage
 
@@ -38,6 +62,17 @@ private class TestMountPage
     mount TestComponentWithArgs, "arg without keyword"
     mount TestComponentWithArgs, title: "arg with keyword"
     mount ComplexTestComponent, title: "arg with keyword"
+    mount ComponentWithBlock, "jane" do |name|
+      text name.upcase
+    end
+    mount ComponentWithBlock, name: "joe" do |name|
+      text name.upcase
+    end
+    mount ComponentWithOptionalBlock
+    mount ComponentWithOptionalBlock do
+      text "Added a block"
+    end
+    @view
   end
 end
 
@@ -49,5 +84,9 @@ describe "mounting a component to a page" do
     contents.should contain("TestComponentWithArgs: arg without keyword")
     contents.should contain("TestComponentWithArgs: arg with keyword")
     contents.should contain("/images/logo-with-hash.png")
+    contents.should contain("JOE")
+    contents.should contain("JANE")
+    contents.should contain("No block given")
+    contents.should contain("Added a block")
   end
 end
--- src/lucky/mount_component.cr
+++ src/lucky/mount_component.cr
@@ -2,4 +2,8 @@ module Lucky::MountComponent
   def mount(component, *args, **named_args)
     component.new(@view, *args, **named_args).render
   end
+
+  def mount(component : T, *args, **named_args, &block : T::RenderableProc) forall T
+    component.new(@view, *args, **named_args, block: block).render
+  end
 end
