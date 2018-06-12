f6b343ad105e0a800b1ba454579d6bd14b2fc032
Paul Smith <paulcsmith0218@gmail.com>
2018-06-12 12:46:00 -0400

    Add helpful message for gen.action
    Closes #503

---
+++ src/precompiled_tasks/gen/action/helpful_error.cr
@@ -0,0 +1,15 @@
+require "colorize"
+
+puts <<-ERROR
+Missing 'browser' or 'api' after 'gen.action'
+
+For actions used in a browser (HTML, redirects)...
+
+    #{"lucky gen.action.browser".colorize.green.bold}
+
+For an API endpoint (JSON, XML, GraphQL)...
+
+    #{"lucky gen.action.api".colorize.green.bold}
+
+
+ERROR
