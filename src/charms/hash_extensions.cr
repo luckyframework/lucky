c3d013e576a2dbced10490482a39e3bc3f3ccee0
Edward Loveall <edward@edwardloveall.com>
2018-06-18 14:45:54 -0400

    Docs for hash extensions (#515)
    * Docs for hash extensions
    
    * Fix hash extension docs
    
    * Update hash extension docs

--- src/charms/hash_extensions.cr
+++ src/charms/hash_extensions.cr
@@ -1,8 +1,33 @@
 class Hash
+  # Return the **nillable** value of a hash key
+  #
+  # This returns a value stored in a hash. The key can be specified as a String
+  # or Symbol. Internally this works by converting Symbols to Strings. See the
+  # code below for an example. It returns `nil` if the value doesn't exist:
+  #
+  # ```crystal
+  # hash = {"name" => "Karin"}
+  # hash.get(:name)  # => "Karin" : (String | Nil)
+  # hash.get("name") # => "Karin" : (String | Nil)
+  # hash.get(:asdf)  # => nil : (String | Nil)
+  # ```
   def get(key : String | Symbol)
     self[key.to_s]?
   end
 
+  # Return the value of a hash key
+  #
+  # This returns a value stored in a hash. The key can be specified as a String
+  # or Symbol. Internally this works by converting Symbols to Strings. See the
+  # code below for an example. It throws a `KeyError` if the value doesn't
+  # exist:
+  #
+  # ```crystal
+  # hash = {"name" => "Karin"}
+  # hash.get(:name)  # => "Karin" : String
+  # hash.get("name") # => "Karin" : String
+  # hash.get(:asdf)  # => KeyError
+  # ```
   def get!(key : String | Symbol)
     self[key.to_s]
   end
