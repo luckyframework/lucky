966be50ccb47dfa1fdd6220a6a5eb60c3a28bcb6
Mikias Abera <itsmikias@gmail.com>
2018-07-20 06:21:20 +0300

    Add enabled setting to LogHandler (#543)
    * Add enabled setting
    
    * Return early if disabled
    
    * Fix handler, add spec
    
    * Run crystal tool format

--- src/lucky/log_handler.cr
+++ src/lucky/log_handler.cr
@@ -5,22 +5,27 @@ class Lucky::LogHandler
 
   Habitat.create do
     setting show_timestamps : Bool
+    setting enabled : Bool = true
   end
 
   def initialize(@io : IO = STDOUT)
   end
 
   def call(context)
-    time = Time.now
-    call_next(context)
-    elapsed = Time.now - time
+    if settings.enabled
+      time = Time.now
+      call_next(context)
+      elapsed = Time.now - time
 
-    if !context.hide_from_logs?
-      log_request(context, time, elapsed)
+      if !context.hide_from_logs?
+        log_request(context, time, elapsed)
+      end
+      {% if !flag?(:release) %}
+        log_debug_messages(context)
+      {% end %}
+    else
+      call_next context
     end
-    {% if !flag?(:release) %}
-      log_debug_messages(context)
-    {% end %}
   rescue e
     log_exception(context, time, e)
     raise e
