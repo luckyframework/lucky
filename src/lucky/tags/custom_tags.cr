40d5b66fa7939290413a5449947e7578056c52f2
Jeremy Woertink <jeremywoertink@gmail.com>
2018-07-24 12:11:17 -0700

    cleanup tons of duplication. Feels a lot better

--- src/lucky/tags/custom_tags.cr
+++ src/lucky/tags/custom_tags.cr
@@ -5,19 +5,6 @@ module Lucky::CustomTags
     name : String,
     content : Lucky::AllowedInTags | String? = "",
     options = EMPTY_HTML_ATTRS,
-    **other_options
-  )
-    merged_options = merge_options(other_options, options)
-
-    tag(name, merged_options) do
-      text content
-    end
-  end
-
-  def tag(
-    name : String,
-    content : Lucky::AllowedInTags | String? = "",
-    options = EMPTY_HTML_ATTRS,
     attrs : Array(Symbol) = [] of Symbol,
     **other_options
   )
@@ -41,18 +28,10 @@ module Lucky::CustomTags
     end
   end
 
-  def tag(name : String, boolean_attrs : String, options = EMPTY_HTML_ATTRS, **other_options, &block)
+  def tag(name : String, boolean_attrs : String = "", options = EMPTY_HTML_ATTRS, **other_options, &block)
     merged_options = merge_options(other_options, options)
     tag_attrs = build_tag_attrs(merged_options)
     @view << "<#{name}" << tag_attrs << boolean_attrs << ">"
-    yield
-    @view << "</#{name}>"
-  end
-
-  def tag(name : String, options = EMPTY_HTML_ATTRS, **other_options, &block)
-    merged_options = merge_options(other_options, options)
-    tag_attrs = build_tag_attrs(merged_options)
-    @view << "<#{name}" << tag_attrs << ">"
     yield
     @view << "</#{name}>"
   end
