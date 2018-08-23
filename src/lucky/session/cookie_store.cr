22d031e97858e40fd3e17b1468210f0b4ffd9df0
Sijawusz Pur Rahnama <sija@sija.pl>
2018-08-23 11:15:06 +0200

    Use Hash composition over inheritance in SessionHash

--- src/lucky/session/cookie_store.cr
+++ src/lucky/session/cookie_store.cr
@@ -1,28 +1,39 @@
 module Lucky::Session
-  class SessionHash < Hash(String, String)
+  class SessionHash
     property changed = false
 
+    @store : Hash(String, String)
+    forward_missing_to @store
+
+    def initialize(pull : JSON::PullParser)
+      @store = Hash(String, String).new(pull)
+    end
+
+    def initialize
+      @store = {} of String => String
+    end
+
     def []=(key : String | Symbol, value)
-      if @changed |= (value != fetch(key.to_s, nil))
+      if @changed |= (value != @store.fetch(key.to_s, nil))
         if value.nil?
-          self.delete(key)
+          @store.delete(key)
         else
-          super(key.to_s, value.to_s)
+          @store[key.to_s] = value.to_s
         end
       end
     end
 
+    def []?(key : Symbol)
+      @store[key.to_s]?
+    end
+
     def [](key)
-      fetch(key.to_s, nil)
+      @store.fetch(key.to_s, nil)
     end
 
     def delete(key)
       @changed = true
-      super(key.to_s)
-    end
-
-    def find_entry(key)
-      super(key.to_s)
+      @store.delete(key.to_s)
     end
   end
 
