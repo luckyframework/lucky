2a4bde281fe7d6b9431095017185540416aee64b
Edward Loveall <edward@edwardloveall.com>
2018-08-25 16:56:03 -0400

    Test the plain adapter
    This reads and writes cookies from a request/response respectively, in
    plain text (albeit encoded). This will be replaced with a version that
    encrypts the cookies, but for now, we're working with this.

--- spec/lucky/cookies/adapters/plain_adapter_spec.cr
+++ spec/lucky/cookies/adapters/plain_adapter_spec.cr
@@ -3,16 +3,32 @@ require "../../../spec_helper"
 include ContextHelper
 
 describe Lucky::Adapters::PlainAdapter do
-  it "writes" do
-    context = build_context
-    cookie_jar = Lucky::CookieJar.new
-    cookie_jar.set :email, "test@example.com"
-    adapter = Lucky::Adapters::PlainAdapter.new
+  describe "#write" do
+    it "writes to the cookie" do
+      context = build_context
+      cookie_jar = Lucky::CookieJar.new
+      cookie_jar.set :email, "test@example.com"
+      adapter = Lucky::Adapters::PlainAdapter.new
 
-    adapter.write("my_key", cookie_jar, to: context.response)
+      adapter.write("my_key", cookie_jar, to: context.response)
 
-    cookies = HTTP::Cookies.from_headers(context.response.headers)
-    cookies["my_key"].value.should eq({"email" => "test@example.com"}.to_json)
+      cookies = HTTP::Cookies.from_headers(context.response.headers)
+      cookies["my_key"].value.should eq({"email" => "test@example.com"}.to_json)
+    end
+
+    it "sets the Set-Cookie header" do
+      context = build_context
+      cookie_jar = Lucky::CookieJar.new
+      cookie_jar.set :email, "test@example.com"
+      encoded = URI.escape({"email" => "test@example.com"}.to_json)
+      adapter = Lucky::Adapters::PlainAdapter.new
+      key = "my_key"
+
+      adapter.write(key, cookie_jar, to: context.response)
+      headers = context.response.headers
+
+      headers["Set-Cookie"].should eq("my_key=#{encoded}; path=/")
+    end
   end
 
   it "reads" do
