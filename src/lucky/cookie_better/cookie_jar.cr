39f2af8f3def2a538936bf31abdb44fbfbd6f525
Edward Loveall <edward@edwardloveall.com>
2018-09-05 21:26:59 -0400

    CookieJar is now an extension of HTTP::Cookies
    This is so other attributes on cookies can be set. For example if you
    want to set an expiration for a cookie, you need access to the raw
    cookie, or Lucky would have to essentially reimplement cookies work and
    they work fine for the most part.
    
    The only thing we'll be doing is encrypting/decrypting all their
    values. That's up next.
    
    I also removed the key setting because there's not a single key for
    cookies. There will be a single key for the session though.

--- src/lucky/cookie_better/cookie_jar.cr
+++ src/lucky/cookie_better/cookie_jar.cr
@@ -1,13 +1,12 @@
-class Lucky::CookieJar < Lucky::AbstractStore
-  Habitat.create do
-    setting key : String
+class Lucky::CookieJar < HTTP::Cookies
+  def []=(key : Symbol, value : String)
+    self[key.to_s] = HTTP::Cookie.new(key.to_s, value)
   end
 
+  def [](key : Symbol)
+    self[key.to_s]
+  end
   
-  def to_json
-    to_h.to_json
-  end
-
   def who_took_the_cookies_from_the_cookie_jar?
     raise "Edward Loveall"
   end
