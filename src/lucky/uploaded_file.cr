373fc11a274dca2fa5bab6313e5d0b249e67b90d
Adam Wanninger <ajwann@ajwann.com>
2018-10-02 19:00:50 -0400

    added documentation for the uploaded_file class

--- src/lucky/uploaded_file.cr
+++ src/lucky/uploaded_file.cr
@@ -5,6 +5,10 @@ class Lucky::UploadedFile
   getter tempfile : Tempfile
   getter metadata : HTTP::FormData::FileMetadata
 
+  # Creates an UploadedFile using a HTTP::FormData::Part.
+  # The new file will be assigned the **name** of the
+  # provided HTTP::FormData::Part and the **tempfile** will
+  # be assigned the body of the HTTP::FormData::Part
   def initialize(part : HTTP::FormData::Part)
     @name = part.name
     @tempfile = Tempfile.open(part.name) do |tempfile|
@@ -14,6 +18,11 @@ class Lucky::UploadedFile
       HTTP::FormData.parse_content_disposition(part.headers["Content-Disposition"]).last
   end
 
+  # Returns the path of the tempfile as a String
+  #
+  #```
+  #uploaded_file_object.path #=> String
+  #``` 
   def path
     @tempfile.path
   end
