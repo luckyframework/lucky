9682c43dca93175ed00b9820f64a0984db44e74f
Sergey Kojin <sergey.kojin@gmail.com>
2019-01-31 00:08:00 +0300

    raise CookieOverflow when any cookie size exceeds the limit
    not when the size of all cookies exceeds the limit

--- src/lucky/session_handler.cr
+++ src/lucky/session_handler.cr
@@ -6,7 +6,6 @@ class Lucky::SessionHandler
 
     write_session(context)
     write_cookies(context)
-    check_cookie_size(context)
   end
 
   private def write_session(context : HTTP::Server::Context) : Void
@@ -23,13 +22,13 @@ class Lucky::SessionHandler
       response.cookies[cookie.name] = cookie
     end
 
-    response.cookies.add_response_headers(response.headers)
-  end
-
-  private def check_cookie_size(context : HTTP::Server::Context) : Void
-    set_cookie_header = context.response.headers["Set-Cookie"]?
-    if set_cookie_header && set_cookie_header.bytesize > Lucky::CookieJar::MAX_COOKIE_SIZE
-      raise Lucky::Exceptions::CookieOverflow.new
+    response.headers.delete("Set-Cookie")
+    response.cookies.each do |cookie|
+      set_cookie_header = cookie.to_set_cookie_header
+      if set_cookie_header && set_cookie_header.bytesize > Lucky::CookieJar::MAX_COOKIE_SIZE
+        raise Lucky::Exceptions::CookieOverflow.new("size of '#{cookie.name}' cookie is too big")
+      end
+      response.headers.add("Set-Cookie", cookie.to_set_cookie_header)
     end
   end
 end
