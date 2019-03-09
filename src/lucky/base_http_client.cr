7480e8f9a82f96897f4d956e1fcddffc4c7e813c
Harrison Bachrach <harrison.bachrach@gmail.com>
2019-03-09 14:36:33 -0800

    Fix `BaseHTTPClient` params (#726)
    The underlying `HTTP::Client` was expecting a host, but we were
    providing it a base URI (which is the combined host and port).
    
    It was also deriving its default value from the `RouteHelper`, but the
    requests are expected to be made to the `Server` *first*.
    
    This also enables the host to be configured without overriding the
    client.

--- src/lucky/base_http_client.cr
+++ src/lucky/base_http_client.cr
@@ -3,8 +3,8 @@ require "http/client"
 class Lucky::BaseHTTPClient
   @client : HTTP::Client
 
-  def initialize(port = nil)
-    @client = HTTP::Client.new(Lucky::RouteHelper.settings.base_uri, port: port)
+  def initialize(host = Lucky::Server.settings.host, port = Lucky::Server.settings.port)
+    @client = HTTP::Client.new(host, port: port)
   end
 
   def get(path : String, headers : HTTP::Headers? = nil, params : HTTP::Params? = nil)
