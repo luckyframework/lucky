2d8e057bccee24659ffde746320085436e8d7e7c
Paul Smith <paulcsmith0218@gmail.com>
2019-03-14 23:25:57 -0400

    JSON

--- src/lucky/params/json_params.cr
+++ src/lucky/params/json_params.cr
@@ -3,9 +3,9 @@ class Lucky::Params::JsonParams < Lucky::Params::BodyParams
     parsed_json.as_h[key]?.try(&.to_s)
   end
 
-  def hash_at?(nested_key : String | Symbol) : Hash(String, String)
+  def hash_at?(key : String | Symbol) : Hash(String, String)
     nested_params = {} of String => String
-    nested_key_json = parsed_json.as_h[nested_key]? || JSON.parse("{}")
+    nested_key_json = parsed_json.as_h[key]? || JSON.parse("{}")
 
     nested_key_json.as_h.each do |key, value|
       nested_params[key.to_s] = value.to_s
@@ -14,30 +14,15 @@ class Lucky::Params::JsonParams < Lucky::Params::BodyParams
     nested_params
   end
 
-  def array_at?(nested_key : String | Symbol) : Array(Hash(String, String))
-    zipped_many_nested_params(nested_key.to_s).map do |a, b|
+  def array_at?(key : String | Symbol) : Array(Hash(String, String))
+    array_of_params(key.to_s).map do |a, b|
       (a || {} of String => String).merge(b || {} of String => String)
     end
   end
 
-  private def zipped_many_nested_params(nested_key : String)
-    body_params = many_nested_body_params(nested_key)
-    query_params = many_nested_query_params(nested_key)
-
-    if body_params.size > query_params.size
-      body_params.zip?(query_params)
-    else
-      query_params.zip?(body_params)
-    end
-  end
-
-  private def many_nested_body_params(nested_key : String) : Array(Hash(String, String))
-    many_nested_json_params(nested_key.to_s)
-  end
-
-  private def many_nested_json_params(nested_key : String) : Array(Hash(String, String))
+  private def array_of_params(key : String) : Array(Hash(String, String))
     many_nested_params = [] of Hash(String, String)
-    nested_key_json = parsed_json.as_h[nested_key]? || JSON.parse("[]")
+    nested_key_json = parsed_json.as_h[key]? || JSON.parse("[]")
 
     nested_key_json.as_a.each do |nested_values|
       nested_params = {} of String => String
@@ -49,10 +34,6 @@ class Lucky::Params::JsonParams < Lucky::Params::BodyParams
     end
 
     many_nested_params
-  end
-
-  private def body_params
-    parsed_json.as_h
   end
 
   @_parsed_json : JSON::Any?
