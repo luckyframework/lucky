97f350302388adc6ec97de86ffa49436b9527d6d
Jeremy Woertink <jeremywoertink@gmail.com>
2019-03-18 16:08:42 -0700

    2 more specs to help keep this thing solid

--- spec/lucky/route_not_found_handler_spec.cr
+++ spec/lucky/route_not_found_handler_spec.cr
@@ -23,4 +23,29 @@ describe Lucky::RouteNotFoundHandler do
   it "has the fallback_action set from a fallback route" do
     Lucky::RouteNotFoundHandler.fallback_action.should eq SampleFallbackAction::Index
   end
+
+  it "responds with a fallback action" do
+    output = IO::Memory.new
+    context = build_context_with_io(output, path: "/non-existent")
+    context.request.method = "GET"
+    handler = Lucky::RouteNotFoundHandler.new
+    handler.next = ->(_ctx : HTTP::Server::Context) {}
+
+    handler.call(context)
+
+    context.response.close
+    output.to_s.should contain "Last chance"
+  end
+
+  it "still raises a Lucky::RouteNotFoundError for non GET requests" do
+    output = IO::Memory.new
+    context = build_context_with_io(output, path: "/non-existent")
+    context.request.method = "POST"
+    handler = Lucky::RouteNotFoundHandler.new
+    handler.next = ->(_ctx : HTTP::Server::Context) {}
+
+    expect_raises(Lucky::RouteNotFoundError) do
+      handler.call(context)
+    end
+  end
 end
