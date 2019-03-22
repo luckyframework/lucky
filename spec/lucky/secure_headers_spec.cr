5c621387ff24c951ee0aea97bdc3523a0960c9b0
Jeremy Woertink <jeremywoertink@gmail.com>
2019-03-22 12:25:32 -0700

    added XSS protection header module

--- spec/lucky/secure_headers_spec.cr
+++ spec/lucky/secure_headers_spec.cr
@@ -50,6 +50,14 @@ class FrameGuardRoutes::WithBadValue < Lucky::Action
   end
 end
 
+class XSSGuardRoutes::Index < Lucky::Action
+  include Lucky::SecureHeaders::SetXSSGuard
+
+  get "/so_custom" do
+    text "test"
+  end
+end
+
 describe Lucky::SecureHeaders do
   describe "SetFrameGuard" do
     it "sets the X-Frame-Options header with sameorigin" do
@@ -71,6 +79,20 @@ describe Lucky::SecureHeaders do
       expect_raises(Exception, "You set frame_guard_value to hax0rz") do
         route = FrameGuardRoutes::WithBadValue.new(build_context, params).call
       end
+    end
+  end
+
+  describe "SetXSSGuard" do
+    it "sets the X-XSS-Protection for a modern browser" do
+      route = XSSGuardRoutes::Index.new(build_context, params).call
+      route.context.response.headers["X-XSS-Protection"].should eq "1; mode=block"
+    end
+
+    it "disables the X-XSS-Protection header on older IE browsers" do
+      request = HTTP::Request.new("GET", "/so_custom")
+      request.headers["User-Agent"] = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)"
+      route = XSSGuardRoutes::Index.new(build_context("/so_custom", request), params).call
+      route.context.response.headers["X-XSS-Protection"].should eq "0"
     end
   end
 end
