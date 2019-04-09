273840838c7de4eb53d7d9fe6baee6a7604311dd
Paul Smith <paulcsmith0218@gmail.com>
2019-04-08 21:59:56 -0400

    Render component comments
    Closes #715

--- src/lucky/mount_component.cr
+++ src/lucky/mount_component.cr
@@ -1,11 +1,19 @@
 module Lucky::MountComponent
   def mount(component : Lucky::BaseComponent) : Nil
+    print_component_comment(component)
     component.view(view).render
   end
 
   def mount(component : Lucky::BaseComponent) : Nil
+    print_component_comment(component)
     component.view(view).render do |*yield_args|
       yield *yield_args
+    end
+  end
+
+  private def print_component_comment(component : Lucky::BaseComponent)
+    if Lucky::HTMLPage.settings.render_component_comments
+      raw "<!-- Rendered by #{component.class.name} -->"
     end
   end
 end
