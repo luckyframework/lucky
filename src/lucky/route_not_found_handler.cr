2e8781ce1641d690df49189e60fc8be82bb9f3e7
Jeremy Woertink <jeremywoertink@gmail.com>
2019-04-15 10:03:47 -0700

    Added some logging when the fallback route picks up a request. (#760)
    * Added some logging when the fallback route picks up a request.
    
    * add some documentation to the RouteNotFoundHandler
    
    * change the logging in RouteNotFoundHandler to use the cool JSON style for pretty formatting

--- src/lucky/route_not_found_handler.cr
+++ src/lucky/route_not_found_handler.cr
@@ -1,9 +1,17 @@
+# This HTTP::Handler takes in the current `context`,
+# then checks to see if a `fallback_action` has been defined to render that action first.
+# If no fallback has been defined, then it will raise a `Lucky::RouteNotFoundError` exception.
+#
+# This handler should be used after the `Lucky::RouteHandler`.
+#
+# See `Lucky::Routeable.fallback` for implementing the `fallback_action`.
 class Lucky::RouteNotFoundHandler
   include HTTP::Handler
   class_property fallback_action : Lucky::Action.class | Nil
 
   def call(context)
     if has_fallback?(context)
+      Lucky.logger.debug(handled_by_fallback: fallback_action.name.to_s)
       fallback_action.new(context, {} of String => String).perform_action
     else
       raise Lucky::RouteNotFoundError.new(context)
