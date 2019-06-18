49afd735640f8441502fabc7b7ef2abf56657e98
Paul Smith <paulcsmith@users.noreply.github.com>
2019-06-18 11:42:32 -0400

    Fix broken spec

--- spec/tasks/build/release_spec.cr
+++ spec/tasks/build/release_spec.cr
@@ -3,10 +3,10 @@ require "../../spec_helper"
 include CleanupHelper
 
 describe Build::Release do
-  it "creates an executable if a `src/server.cr` file exists" do
+  it "creates an executable if a `src/start_server.cr` file exists" do
     with_cleanup do
       Dir.mkdir_p("./src")
-      File.write "./src/server.cr", "puts 1"
+      File.write "./src/start_server.cr", "puts 1"
 
       Build::Release.new(IO::Memory.new).call
 
@@ -17,11 +17,11 @@ describe Build::Release do
   it "does not create executable if the build fails" do
     with_cleanup do
       Dir.mkdir_p("./src")
-      File.write "./src/server.cr", %({{ raise "this build will fail" }})
+      File.write "./src/start_server.cr", %({{ raise "this build will fail" }})
 
       Build::Release.new(IO::Memory.new, error_io: IO::Memory.new).call
 
-      File.exists?("./server").should be_false
+      File.exists?("./start_server").should be_false
     end
   end
 end
