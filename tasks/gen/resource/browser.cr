31848d916bdba9d2e6333e508ae2e95d9788263a
Paul Smith <paulcsmith0218@gmail.com>
2019-09-09 12:02:52 -0400

    Fix resource migration

--- tasks/gen/resource/browser.cr
+++ tasks/gen/resource/browser.cr
@@ -50,8 +50,8 @@ class Gen::Resource::Browser < LuckyCli::Task
   private def migrate_contents : String
     String.build do |string|
       string << "create table_for(#{resource_name}) do\n"
-      string << "  primary_key id : Int64"
-      string << "  add_timestamps"
+      string << "  primary_key id : Int64\n"
+      string << "  add_timestamps\n"
       columns.each do |column|
         string << "  add #{column.name} : #{column.type}\n"
       end
