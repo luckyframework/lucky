d32a46bb755f2f9b8f576f22b3309052ef2a9975
Paul Smith <paulcsmith0218@gmail.com>
2019-09-17 17:35:33 -0400

    Make invalid_attribute_name public

--- spec/lucky/invalid_operation_error_spec.cr
+++ spec/lucky/invalid_operation_error_spec.cr
@@ -14,7 +14,8 @@ describe Avram::InvalidOperationError do
     error = Avram::InvalidOperationError.new(operation)
 
     error.should be_a(Lucky::RenderableError)
-    error.renderable_status.should eq 400
+    error.invalid_attribute_name.should eq("name")
+    error.renderable_status.should eq(400)
     error.renderable_message.should contain("Invalid params")
     error.renderable_details.should eq("name is required")
   end
