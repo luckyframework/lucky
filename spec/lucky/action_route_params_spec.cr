9b8ac5eca62bed21d40c1f7dfd6a1cf7a89c1289
Wesley Moore <wes@wezm.net>
2019-10-03 10:35:26 +0800

    Add test for route_params for routes that differ on extension

--- spec/lucky/action_route_params_spec.cr
+++ spec/lucky/action_route_params_spec.cr
@@ -17,3 +17,30 @@ describe "Automatically generated param helpers" do
     typeof(action.param_2).should eq String
   end
 end
+
+# Test for https://github.com/luckyframework/lucky/issues/928
+private class TestRssFeedAction < TestAction
+  get "/:test/feed.rss" do
+    plain_text "rss #{test}"
+  end
+end
+
+private class TestJsonFeedAction < TestAction
+  get "/:other/feed.json" do
+    plain_text "json #{other}"
+  end
+end
+
+describe "Routes that differ in file extension only" do
+  it "sets route params properly" do
+    Lucky::Router.add :get, "/:test/feed.rss", TestRssFeedAction
+    Lucky::Router.add :get, "/:other/feed.json", TestJsonFeedAction
+
+    handler = Lucky::Router.find_action(:get, "/prefix/feed.json")
+    if handler
+      handler.params.should eq({"other" => "prefix"})
+    else
+      handler.should_not be_nil
+    end
+  end
+end
