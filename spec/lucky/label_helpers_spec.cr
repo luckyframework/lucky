521647f3944b07501547d702b3e2e6c7f66ce4e7
Paul Smith <paulcsmith0218@gmail.com>
2020-03-14 11:25:44 -0400

    Revert "[Breaking change] Remove label text inference from label_for"
    This reverts commit b41fe3c2749d1f49b3eecea0767f5a8b2f82ab53.

--- spec/lucky/label_helpers_spec.cr
+++ spec/lucky/label_helpers_spec.cr
@@ -1,7 +1,5 @@
 require "../spec_helper"
 
-include ContextHelper
-
 class TestUser
   def first_name
     "My Name"
@@ -26,11 +24,19 @@ private class TestPage
   end
 
   def label_without_html_options
-    label_for form.first_name, "First name"
+    label_for form.first_name
   end
 
   def label_with_html_options
-    label_for form.first_name, "First name", class: "best-label"
+    label_for form.first_name, class: "best-label"
+  end
+
+  def label_with_custom_text
+    label_for form.first_name, "My Label"
+  end
+
+  def label_with_custom_text_and_options
+    label_for form.first_name, "My Label", class: "best-label"
   end
 
   def label_with_block
@@ -53,6 +59,14 @@ describe Lucky::LabelHelpers do
 
     view.label_with_html_options.to_s.should contain <<-HTML
     <label for="user_first_name" class="best-label">First name</label>
+    HTML
+
+    view.label_with_custom_text.to_s.should contain <<-HTML
+    <label for="user_first_name">My Label</label>
+    HTML
+
+    view.label_with_custom_text_and_options.to_s.should contain <<-HTML
+    <label for="user_first_name" class="best-label">My Label</label>
     HTML
 
     view.label_with_block.to_s.should contain <<-HTML
--- src/lucky/tags/label_helpers.cr
+++ src/lucky/tags/label_helpers.cr
@@ -1,4 +1,12 @@
 module Lucky::LabelHelpers
+  def label_for(field : Avram::PermittedAttribute, **html_options)
+    label_for(
+      field,
+      Wordsmith::Inflector.humanize(field.name.to_s),
+      **html_options
+    )
+  end
+
   def label_for(field : Avram::PermittedAttribute, text : String, **html_options)
     label(
       text,
@@ -10,19 +18,6 @@ module Lucky::LabelHelpers
     label(merge_options(html_options, {"for" => input_id(field)})) do
       yield
     end
-  end
-
-  def label_for(field : Avram::PermittedAttribute, **options)
-    {% raise <<-ERROR
-      'label_for' no longer guesses the label text. Please provide the text for this label.
-
-      Try this...
-
-        ▸ label_for operation.my_attribute, "This is the label text"
-
-
-      ERROR
-    %}
   end
 
   def label_for(field, **options)
