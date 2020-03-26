8a9a9cc6314c84d812d2a5337ae4cc7dbe59ee68
Paul Smith <paulcsmith0218@gmail.com>
2020-03-26 17:11:09 -0400

    Add compiling spinner to watch task
    This does not work yet. We need to point the other shards in Lucky to
    use LuckyCli master. Since they don't I just brought in the Spinner for
    testing.
    
    The spinner works fine when running `lucky watch` directly. But when run
    inside of overmind it shows nothing...not sure why

--- tasks/watch.cr
+++ tasks/watch.cr
@@ -5,6 +5,134 @@ require "yaml"
 require "../src/lucky/server_settings"
 require "option_parser"
 
+# Adding this in From LuckyCli for now
+class LuckyCli::Spinner
+  DELETE_LINE = "\33[2K\r"
+
+  private property frame_index : Int32 = 0
+  private property? started : Bool = false
+  private property last_frame_rendered_at : Time? = nil
+
+  @done_text : String | ColoredString | Nil
+  @start_text : String | ColoredString
+  getter colorize_frame : Proc(String, ColoredString)
+  getter io : IO
+  delegate frames, to: self.class
+  class_getter frames = [
+    "⣾",
+    "⣽",
+    "⣻",
+    "⢿",
+    "⡿",
+    "⣟",
+    "⣯",
+    "⣷",
+  ]
+
+  alias ColoredString = Colorize::Object(String)
+
+  def initialize(
+    @start_text,
+    @io = STDERR,
+    @done_text : String | ColoredString | Nil = nil,
+    @colorize_frame = ->(frame : String) { frame.colorize.bold.green }
+  )
+  end
+
+  def self.start(*args, **named_args)
+    spinner = new(*args, **named_args).start
+    yield.tap do |last_value_from_block|
+      spinner.stop
+    end
+  end
+
+  def start : self
+    self.started = true
+    print_spinner
+
+    spawn do
+      while started?
+        if ready_for_next_frame?
+          increment_frame
+          print_spinner
+        end
+        Fiber.yield
+      end
+    end
+
+    self
+  end
+
+  def stop : self
+    self.started = false
+    delete_line
+    io.print start_text
+    io.print done_text
+    self
+  end
+
+  private def ready_for_next_frame? : Bool
+    if rendered_at = last_frame_rendered_at
+      rendered_at < 100.milliseconds.ago
+    else
+      # Rendering first frame
+      true
+    end
+  end
+
+  def delete_line
+    io.print DELETE_LINE
+  end
+
+  private def increment_frame : Nil
+    if last_frame?
+      self.frame_index = 0
+    else
+      self.frame_index += 1
+    end
+  end
+
+  private def last_frame? : Bool
+    frame_index == (frames.size - 1)
+  end
+
+  private def print_spinner : Nil
+    delete_line
+    io.print start_text
+    io.print " #{current_frame} "
+    self.last_frame_rendered_at = Time.utc
+  end
+
+  private def current_frame : String | ColoredString
+    colorize_frame.call(frames[frame_index])
+  end
+
+  private def start_text : ColoredString
+    text = @start_text
+    if text.is_a?(ColoredString)
+      text
+    else
+      # Default start text color to bold
+      text.colorize.bold
+    end
+  end
+
+  private def done_text : String | ColoredString
+    if @done_text
+      text = if @done_text.is_a?(ColoredString)
+               @done_text
+             else
+               # Default done color to bold green
+               @done_text.colorize.bold.green
+             end
+
+      " #{text}\n"
+    else
+      DELETE_LINE
+    end
+  end
+end
+
 # Based on the sentry shard with some modifications to output and build process.
 module LuckySentry
   FILE_TIMESTAMPS  = {} of String => String # {file => timestamp}
@@ -145,7 +273,6 @@ module LuckySentry
 
     def start_app
       stop_all_processes
-      puts "Compiling..."
       start_all_processes
     end
 
@@ -156,10 +283,16 @@ module LuckySentry
     end
 
     private def start_all_processes
-      if build_app_processes.all? &.success?
+      successful_build = LuckyCli::Spinner.start("Compiling...") do
+        sleep(2)
+      end
+      successful_build = LuckyCli::Spinner.start("Compiling...") do
+        build_app_processes.all? &.success?
+      end
+
+      if successful_build
         self.app_built = true
         create_app_processes()
-        puts "Done compiling"
       elsif !app_built
         print_error_message
       end
@@ -224,8 +357,6 @@ class Watch < LuckyCli::Task
       run_commands: run_commands,
       reload_browser: @reload_browser
     )
-
-    puts "Beginning to watch your project"
 
     loop do
       process_runner.scan_files
