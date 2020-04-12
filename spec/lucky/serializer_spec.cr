4f60eeb5adbd1a0618bbdb28f8e31c947c619392
Hannes Käufler <hanneskaeufler@users.noreply.github.com>
2020-04-13 00:27:06 +0200

    Serialize UUIDs by default, fixes #1069 (#1103)
    It's actually just a matter of using the right include,
    but since I had to hunt that down in the docs as well
    (https://crystal-lang.org/api/0.34.0/UUID.html#new(pull:JSON::PullParser)-class-method)
    I'd say it is a win for lucky to include that for the serializer
    by default.

--- spec/lucky/serializer_spec.cr
+++ spec/lucky/serializer_spec.cr
@@ -18,6 +18,15 @@ private class TestNestedJSON < Lucky::Serializer
   end
 end
 
+private class TestUuidJSON < Lucky::Serializer
+  def initialize(@id : UUID)
+  end
+
+  def render
+    {id: @id}
+  end
+end
+
 describe Lucky::Serializer do
   describe "#to_json" do
     it "calls to_json on the render data" do
@@ -27,6 +36,11 @@ describe Lucky::Serializer do
     it "handles nested JSON classes" do
       nested = TestNestedJSON.new(user_name: "Picard")
       nested.to_json.should eq({user: {name: "Picard"}}.to_json)
+    end
+
+    it "handles UUIds without user's having to include uuid/json manually" do
+      id = UUID.new("87b3042b-9b9a-41b7-8b15-a93d3f17025e")
+      TestUuidJSON.new(id).to_json.should eq({id: id.to_s}.to_json)
     end
   end
 end
--- src/lucky/serializer.cr
+++ src/lucky/serializer.cr
@@ -1,3 +1,5 @@
+require "uuid/json"
+
 abstract class Lucky::Serializer
   def to_json(io)
     render.to_json(io)
