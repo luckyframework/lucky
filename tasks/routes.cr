b9f4fc1898e3ad4838a8ef27a95f8c7b7458cec0
Jeremy Woertink <jeremywoertink@gmail.com>
2020-05-01 08:46:55 -0700

    Implementing a new UI for lucky routes  (#1128)
    * Implementing a new UI for lucky routes that includes a route helper example, and query params if they're declared in your action. Fixes #1070
    
    * working out design for the routes UI
    
    * Update tasks/routes.cr
    
    Co-authored-by: Paul Smith <paulcsmith@users.noreply.github.com>
    
    * ameba
    
    Co-authored-by: Paul Smith <paulcsmith@users.noreply.github.com>

--- tasks/routes.cr
+++ tasks/routes.cr
@@ -1,21 +1,34 @@
 require "lucky_cli"
 require "colorize"
-require "shell-table"
 
 class Routes < LuckyCli::Task
   summary "Show all the routes for the app"
 
   def call
-    routes = Lucky::Router.routes.map do |route|
-      [route.method.to_s.upcase, route.path.colorize.green, route.action]
+    table = String.build do |output|
+      output << "#{print_banner_message}\n\n"
+      Lucky::Router.routes.map do |route|
+        output << "#{route.method.to_s.upcase} #{route.path.colorize.bold.underline} #{dim_arrow} #{route.action.colorize.green}\n"
+        route.action.query_param_declarations.each do |param|
+          output << " #{dim_arrow} #{param}\n"
+        end
+        output << "\n\n"
+      end
     end
 
-    table = ShellTable.new(
-      labels: ["Verb", "URI", "Action"],
-      label_color: :blue,
-      rows: routes
-    )
-
     puts table
+  end
+
+  private def dim_arrow
+    "▸".colorize.dim
+  end
+
+  private def print_banner_message
+    <<-TEXT.colorize.dim
+
+    Routing documentation:
+
+      https://luckyframework.org/guides/http-and-routing/routing-and-params
+    TEXT
   end
 end
