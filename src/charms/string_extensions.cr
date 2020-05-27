e0e17bba54395b2eb8c89de12908c3d69922e2bb
James Cook <jcook.rubyist@gmail.com>
2020-05-26 20:04:54 -0500

    Faster String#squish
      squish original ascii-only whitespace (600 bytes)  42.92k ( 23.30µs) (± 0.55%)  5.92kB/op   8.58× slower
     squish optimized ascii-only whitespace (600 bytes) 368.48k (  2.71µs) (± 3.49%)  1.19kB/op        fastest
    
      squish original w/ unicode whitespace (60 bytes) 528.12k (  1.89µs) (± 2.31%)  624B/op   3.39× slower
    squish optimized w/ unicode whitespace (60 bytes)   1.79M (557.76ns) (± 2.61%)  225B/op        fastest

--- src/charms/string_extensions.cr
+++ src/charms/string_extensions.cr
@@ -10,6 +10,45 @@ class String
   # replace newlines with a single space and convert mutiple spaces to just one
   # space.
   def squish : String
-    gsub(/[[:space:]]+/, " ").strip
+    if ascii_only?
+      squish_ascii
+    else
+      squish_unicode
+    end
+  end
+
+  # Optimized for ASCII using String#ascii_whitespace?
+  private def squish_ascii : String
+    String.build(size) do |str|
+      print_blank = false
+      each_char do |chr|
+        if chr.ascii_whitespace?
+          if print_blank
+            str << ' '
+            print_blank = false
+          end
+        else
+          print_blank = true
+          str << chr
+        end
+      end
+    end.strip
+  end
+
+  private def squish_unicode : String
+    String.build(size) do |str|
+      print_blank = false
+      each_char do |chr|
+        if chr.whitespace?
+          if print_blank
+            str << ' '
+            print_blank = false
+          end
+        else
+          print_blank = true
+          str << chr
+        end
+      end
+    end.strip
   end
 end
