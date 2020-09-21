b83b9f24ca5bf294b3806ec2268ab96d944fab6c
Stephen Dolan <dolan.stephen1@gmail.com>
2020-09-21 12:01:17 -0400

    Rename with_defaults to tag_defaults (#1262)
    This PR aims to close #1252
    
    Tackled in this PR:
    - Renamed definition and spec file names
    - Changed method call to `tag_defaults`, and yielded block to `tag_builder` everywhere
    - Changed HtmlBuilder inclusion to reference new file name

--- spec/lucky/tag_defaults_spec.cr
+++ spec/lucky/tag_defaults_spec.cr
@@ -1,45 +1,45 @@
 require "../spec_helper"
 
-private class TestWithDefaultsPage
+private class TestTagDefaultsPage
   include Lucky::HTMLPage
 
   def render
-    with_defaults field: name_field, class: "form-control" do |html|
-      html.text_input
+    tag_defaults field: name_field, class: "form-control" do |tag_builder|
+      tag_builder.text_input
     end
 
-    with_defaults field: name_field, class: "form-control" do |html|
-      html.text_input placeholder: "Name please"
+    tag_defaults field: name_field, class: "form-control" do |tag_builder|
+      tag_builder.text_input placeholder: "Name please"
     end
 
-    with_defaults field: name_field, placeholder: "default" do |html|
-      html.text_input placeholder: "replace default"
+    tag_defaults field: name_field, placeholder: "default" do |tag_builder|
+      tag_builder.text_input placeholder: "replace default"
     end
 
-    with_defaults field: name_field, class: "default" do |html|
-      html.text_input append_class: "appended classes"
+    tag_defaults field: name_field, class: "default" do |tag_builder|
+      tag_builder.text_input append_class: "appended classes"
     end
 
-    with_defaults field: name_field, class: "default" do |html|
-      html.text_input replace_class: "replaced"
+    tag_defaults field: name_field, class: "default" do |tag_builder|
+      tag_builder.text_input replace_class: "replaced"
     end
 
     # No default 'class'
-    with_defaults field: name_field do |html|
-      html.text_input append_class: "appended-without-default"
+    tag_defaults field: name_field do |tag_builder|
+      tag_builder.text_input append_class: "appended-without-default"
     end
 
-    with_defaults field: name_field do |html|
-      html.text_input replace_class: "replaced-without-default"
+    tag_defaults field: name_field do |tag_builder|
+      tag_builder.text_input replace_class: "replaced-without-default"
     end
 
     # tags that have content
-    with_defaults class: "default" do |html|
-      html.div "text content"
+    tag_defaults class: "default" do |tag_builder|
+      tag_builder.div "text content"
     end
 
-    with_defaults class: "default" do |html|
-      html.div do
+    tag_defaults class: "default" do |tag_builder|
+      tag_builder.div do
         text "block content"
       end
     end
@@ -48,9 +48,9 @@ private class TestWithDefaultsPage
   end
 end
 
-describe "with_defaults" do
+describe "tag_defaults" do
   it "renders the component" do
-    contents = TestWithDefaultsPage.new(build_context).render.to_s
+    contents = TestTagDefaultsPage.new(build_context).render.to_s
 
     contents
       .should contain %(<input type="text" id="user_name" name="user:name" value="" class="form-control">)
