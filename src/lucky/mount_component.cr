d36cf2d3612e5885c1c501b57e69f9886cc38066
Michael Miller <icy.arctic.fox@gmail.com>
2021-03-30 15:12:45 -0600

    Mount instance (#1446)
    * Add `mount_instance` variant of `mount`
    
    * Add tests for `mount_instance`
    
    * Add compiler hints for mount and mount_instance

--- src/lucky/mount_component.cr
+++ src/lucky/mount_component.cr
@@ -73,6 +73,97 @@ module Lucky::MountComponent
     end
   end
 
+  # :nodoc:
+  def mount(_component : Lucky::BaseComponent, *args, **named_args) : Nil
+    {% raise <<-ERROR
+        'mount' requires a component class, not an instance of a component.
+
+        Try this...
+
+           ▸ mount MyComponent
+           ▸ mount_instance MyComponent.new
+        ERROR
+    %}
+  end
+
+  # :nodoc:
+  def mount(_component : Lucky::BaseComponent, *args, **named_args, &) : Nil
+    {% raise <<-ERROR
+        'mount' requires a component class, not an instance of a component.
+
+        Try this...
+
+           ▸ mount MyComponent
+           ▸ mount_instance MyComponent.new
+        ERROR
+    %}
+  end
+
+  # :nodoc:
+  def mount_instance(_component : Lucky::BaseComponent.class) : Nil
+    {% raise <<-ERROR
+        'mount_instance' requires an instance of a component, not component class.
+
+        Try this...
+
+           ▸ mount MyComponent
+           ▸ mount_instance MyComponent.new
+        ERROR
+    %}
+  end
+
+  # :nodoc:
+  def mount_instance(_component : Lucky::BaseComponent.class, &) : Nil
+    {% raise <<-ERROR
+        'mount_instance' requires an instance of a component, not component class.
+
+        Try this...
+
+           ▸ mount MyComponent
+           ▸ mount_instance MyComponent.new
+        ERROR
+    %}
+  end
+
+  # Appends the `component` to the view.
+  # The `component` is a previously initialized instance of a component.
+  #
+  # When `Lucky::HTMLPage.settings.render_component_comments` is
+  # set to `true`, it will render HTML comments showing where the component
+  # starts and ends.
+  #
+  # ```
+  # component = MyComponent.new(name: "Jane")
+  # mount_instance(component)
+  # ```
+  def mount_instance(component : Lucky::BaseComponent) : Nil
+    print_component_comment(component.class) do
+      component.view(view).render
+    end
+  end
+
+  # Appends the `component` to the view. Takes a block, and yields the
+  # args passed to the component.
+  # The `component` is a previously initialized instance of a component.
+  #
+  # When `Lucky::HTMLPage.settings.render_component_comments` is
+  # set to `true`, it will render HTML comments showing where the component
+  # starts and ends.
+  #
+  # ```
+  # component = MyComponent.new(name: "Jane")
+  # mount_instance(component) do |name|
+  #   text name.upcase
+  # end
+  # ```
+  def mount_instance(component : Lucky::BaseComponent) : Nil
+    print_component_comment(component.class) do
+      component.view(view).render do |*yield_args|
+        yield *yield_args
+      end
+    end
+  end
+
   private def print_component_comment(component : Lucky::BaseComponent.class) : Nil
     if Lucky::HTMLPage.settings.render_component_comments
       raw "<!-- BEGIN: #{component.name} #{component.file_location} -->"
