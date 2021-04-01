a86ba587cfc257ad297588dcf37a3f50d80b3679
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2021-03-31 22:05:23 -0400

    Fix spec to work on Crystal 0.36.1 and 1.0.0

--- spec/lucky/cookies/cookie_jar_spec.cr
+++ spec/lucky/cookies/cookie_jar_spec.cr
@@ -30,14 +30,16 @@ describe Lucky::CookieJar do
     jar.get_raw?("missing").should be_nil
   end
 
-  it "raises a nicer error for invalid cookie values" do
-    value = "Double Chocolate"
-    jar = Lucky::CookieJar.empty_jar
+  {% if compare_versions(Crystal::VERSION, "0.36.1") > 0 %}
+    it "raises a nicer error for invalid cookie values" do
+      value = "Double Chocolate"
+      jar = Lucky::CookieJar.empty_jar
 
-    expect_raises(Lucky::InvalidCookieValueError, "Cookie value for 'cookie' is invalid") do
-      jar.set_raw("cookie", value)
+      expect_raises(Lucky::InvalidCookieValueError, "Cookie value for 'cookie' is invalid") do
+        jar.set_raw("cookie", value)
+      end
     end
-  end
+  {% end %}
 
   it "raises CookieNotFoundError when getting a raw cookie that doesn't exist" do
     jar = Lucky::CookieJar.empty_jar
@@ -89,7 +91,11 @@ describe Lucky::CookieJar do
         message = jar.get_raw(:message)
         message.http_only.should be_true
         message.expires.should be_nil
-        message.path.should be_nil
+        {% if compare_versions(Crystal::VERSION, "0.36.1") > 0 %}
+          message.path.should be_nil
+        {% else %}
+          message.path.should eq "/"
+        {% end %}
         message.domain.should be_nil
         message.secure.should be_false
       end
@@ -203,9 +209,13 @@ describe Lucky::CookieJar do
     it "deletes cookies with options" do
       headers = HTTP::Headers.new
       headers["Cookie"] = "name=Rick%20James"
+      cookies = {% if compare_versions(Crystal::VERSION, "0.36.1") > 0 %}
+                  HTTP::Cookies.from_client_headers(headers)
+                {% else %}
+                  HTTP::Cookies.from_headers(headers)
+                {% end %}
 
-      jar = Lucky::CookieJar.from_request_cookies(
-        HTTP::Cookies.from_client_headers(headers))
+      jar = Lucky::CookieJar.from_request_cookies(cookies)
 
       jar.clear do |cookie|
         cookie.path("/")
