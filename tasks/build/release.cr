d6b16a1fe0da4ad8036411abf64396bcd526ad27
Jeremy Woertink <jeremywoertink@gmail.com>
2021-04-02 16:15:44 -0700

    Update the build.release task (#1454)
    * Updating the release task to use the latest LuckyCli out, and build to the bin dir. Fixes #1387
    
    * Fix output in spec

--- tasks/build/release.cr
+++ tasks/build/release.cr
@@ -6,20 +6,19 @@ class Build::Release < LuckyCli::Task
 
   summary "Compile app for production"
 
-  def initialize(@io : IO = STDOUT, @error_io : IO = STDERR)
-  end
+  property error_io : IO = STDERR
 
   def call
-    command = "crystal build --release src/start_server.cr"
+    command = "crystal build --release src/start_server.cr -o bin/start_server"
 
     log "Building binary with '#{command}"
-    process = Process.run(command, shell: true, output: @io, error: @error_io)
+    process = Process.run(command, shell: true, output: output, error: error_io)
     if process.success?
-      log "Build succeeded - binary saved at './start_server'"
+      log "Build succeeded - binary saved at './bin/start_server'"
     end
   end
 
   def log(message)
-    @io.puts "  #{green_arrow} #{message}"
+    output.puts "  #{green_arrow} #{message}"
   end
 end
