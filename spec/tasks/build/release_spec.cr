adec74ba3773c5cae2f86b4520ccfec3cba96798
Jeremy Woertink <jeremywoertink@gmail.com>
2021-04-02 14:43:03 -0700

    Updating the release task to use the latest LuckyCli out, and build to the bin dir. Fixes #1387

--- spec/tasks/build/release_spec.cr
+++ spec/tasks/build/release_spec.cr
@@ -6,22 +6,31 @@ describe Build::Release do
   it "creates an executable if a `src/start_server.cr` file exists" do
     with_cleanup do
       Dir.mkdir_p("./src")
+      Dir.mkdir_p("./bin")
       File.write "./src/start_server.cr", "puts 1"
 
-      Build::Release.new(IO::Memory.new).call
+      task = Build::Release.new
+      task.output = IO::Memory.new
+      task.call
+      task.output.to_s.should contain("Build succeeded")
 
-      File.exists?("./start_server").should be_true
+      File.exists?("./bin/start_server").should be_true
     end
   end
 
   it "does not create executable if the build fails" do
     with_cleanup do
       Dir.mkdir_p("./src")
+      Dir.mkdir_p("./bin")
       File.write "./src/start_server.cr", %({{ raise "this build will fail" }})
 
-      Build::Release.new(IO::Memory.new, error_io: IO::Memory.new).call
+      task = Build::Release.new
+      task.error_io = IO::Memory.new
+      task.call
+      task.error_io.to_s.should contain("this build will fail")
+      task.output.to_s
 
-      File.exists?("./start_server").should be_false
+      File.exists?("./bin/start_server").should be_false
     end
   end
 end
