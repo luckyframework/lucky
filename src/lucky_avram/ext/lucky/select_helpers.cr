e79b85a05f072d2d6c0a4df4e36e5cc8acca7155
Jeremy Woertink <jeremywoertink@gmail.com>
2021-06-02 14:32:12 -0700

    Adding in new method for using arrays with select_tag. Fixes #777

--- src/lucky_avram/ext/lucky/select_helpers.cr
+++ src/lucky_avram/ext/lucky/select_helpers.cr
@@ -5,6 +5,12 @@ module Lucky::SelectHelpers
     end
   end
 
+  def multi_select_input(field : Avram::PermittedAttribute, **html_options) : Nil
+    select_tag [:multiple], merge_options(html_options, {"name" => input_name(field, array: true)}) do
+      yield
+    end
+  end
+
   def options_for_select(field : Avram::PermittedAttribute(T), select_options : Array(Tuple(String, T?)), **html_options) : Nil forall T
     select_options.each do |option_name, option_value|
       attributes = {"value" => option_value.to_s}
@@ -16,13 +22,29 @@ module Lucky::SelectHelpers
     end
   end
 
+  def options_for_multi_select(field : Avram::PermittedAttribute(Array(T)), select_options : Array(Tuple(String, T)), **html_options) : Nil forall T
+    select_options.each do |option_name, option_value|
+      attributes = {"value" => option_value.to_s}
+
+      if value = field.value
+        is_selected = value.includes?(option_value.to_s)
+        attributes["selected"] = "true" if is_selected
+      end
+
+      option(option_name, merge_options(html_options, attributes))
+    end
+  end
+
   # Renders an <option> HTML tag with no value
   # The text is set to `label`.
   def select_prompt(label : String) : Nil
     option(label, value: "")
   end
 
-  private def input_name(field)
-    "#{field.param_key}:#{field.name}"
+  private def input_name(field, *, array : Bool = false)
+    String.build do |name|
+      name << "#{field.param_key}:#{field.name}"
+      name << "[]" if array
+    end
   end
 end
