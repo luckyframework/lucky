86a5ebaa28cec83a25982d284081c38e5758fc7e
Jeremy Woertink <jeremywoertink@gmail.com>
2021-06-09 13:02:16 -0700

    Add new multi_select_input tag (#1518)
    * Adding in new method for using arrays with select_tag. Fixes #777
    
    * add specs and fix the selected attribute
    
    * Use override method on options_for_select. Updated multi_select to ensure an Array is passed in

--- src/lucky_avram/ext/lucky/select_helpers.cr
+++ src/lucky_avram/ext/lucky/select_helpers.cr
@@ -5,6 +5,12 @@ module Lucky::SelectHelpers
     end
   end
 
+  def multi_select_input(field : Avram::PermittedAttribute(Array), **html_options) : Nil
+    select_tag [:multiple], merge_options(html_options, {"name" => input_name(field)}) do
+      yield
+    end
+  end
+
   def options_for_select(field : Avram::PermittedAttribute(T), select_options : Array(Tuple(String, T?)), **html_options) : Nil forall T
     select_options.each do |option_name, option_value|
       attributes = {"value" => option_value.to_s}
@@ -13,6 +19,20 @@ module Lucky::SelectHelpers
       attributes["selected"] = "true" if is_selected
 
       option(option_name, merge_options(html_options, attributes))
+    end
+  end
+
+  def options_for_select(field : Avram::PermittedAttribute(Array(T)), select_options : Array(Tuple(String, T)), **html_options) : Nil forall T
+    select_options.each do |option_name, option_value|
+      attributes = {"value" => option_value.to_s}
+      bool_attrs = [] of Symbol
+
+      if value = field.value
+        is_selected = value.includes?(option_value.to_s)
+        bool_attrs << :selected if is_selected
+      end
+
+      option(option_name, attrs: bool_attrs, options: merge_options(html_options, attributes))
     end
   end
 
