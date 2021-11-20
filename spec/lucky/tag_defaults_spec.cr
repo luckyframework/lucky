d41619ec82ba2201a13d458d24e1be9e220ac227
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2021-11-20 15:53:18 -0600

    Fix specs to cut dependency on avram integration

--- spec/lucky/tag_defaults_spec.cr
+++ spec/lucky/tag_defaults_spec.cr
@@ -4,53 +4,41 @@ private class TestTagDefaultsPage
   include Lucky::HTMLPage
 
   def render
-    tag_defaults field: name_field, class: "form-control" do |tag_builder|
-      tag_builder.text_input
+    tag_defaults do |tag_builder|
+      tag_builder.div "text content"
     end
 
-    tag_defaults field: name_field, class: "form-control" do |tag_builder|
-      tag_builder.text_input placeholder: "Name please"
-    end
-
-    tag_defaults field: name_field, placeholder: "default" do |tag_builder|
-      tag_builder.text_input placeholder: "replace default"
-    end
-
-    tag_defaults field: name_field, class: "default" do |tag_builder|
-      tag_builder.text_input append_class: "appended classes"
-    end
-
-    tag_defaults field: name_field, class: "default" do |tag_builder|
-      tag_builder.text_input replace_class: "replaced"
-    end
-
-    # No default 'class'
-    tag_defaults field: name_field do |tag_builder|
-      tag_builder.text_input append_class: "appended-without-default"
-    end
-
-    tag_defaults field: name_field do |tag_builder|
-      tag_builder.text_input replace_class: "replaced-without-default"
-    end
-
-    # tags that have content
     tag_defaults class: "default" do |tag_builder|
       tag_builder.div "text content"
     end
 
     tag_defaults class: "default" do |tag_builder|
+      tag_builder.div "text content", append_class: "appended classes"
+    end
+
+    tag_defaults class: "default" do |tag_builder|
+      tag_builder.div "text content", replace_class: "replaced"
+    end
+
+    tag_defaults id: "foo" do |tag_builder|
+      tag_builder.div "text content", append_class: "appended-without-default"
+    end
+
+    tag_defaults id: "foo" do |tag_builder|
+      tag_builder.div "text content", replace_class: "replaced-without-default"
+    end
+
+    tag_defaults do |tag_builder|
       tag_builder.div do
         text "block content"
       end
     end
 
-    {% if compare_versions(Crystal::VERSION, "0.36.1") > 0 %}
-      tag_defaults do |tag_builder|
-        tag_builder.div "@click": "onclick($event)" do
-          text "block content"
-        end
+    tag_defaults do |tag_builder|
+      tag_builder.div "@click": "onclick($event)" do
+        text "block content"
       end
-    {% end %}
+    end
 
     view
   end
@@ -62,36 +50,13 @@ describe "tag_defaults" do
   it "renders the component" do
     contents = TestTagDefaultsPage.new(build_context).render.to_s
 
-    contents
-      .should contain %(<input type="text" id="user_name" name="user:name" value="" class="form-control">)
-    contents
-      .should contain %(<input type="text" id="user_name" name="user:name" value="" class="form-control" placeholder="Name please">)
-    contents
-      .should contain %(<input type="text" id="user_name" name="user:name" value="" placeholder="replace default">)
-    contents
-      .should contain %(<input type="text" id="user_name" name="user:name" value="" class="default appended classes">)
-    contents
-      .should contain %(<input type="text" id="user_name" name="user:name" value="" class="replaced">)
-    contents
-      .should contain %(<input type="text" id="user_name" name="user:name" value="" class="appended-without-default">)
-    contents
-      .should contain %(<input type="text" id="user_name" name="user:name" value="" class="replaced-without-default">)
-    contents
-      .should contain %(<div class="default">text content</div>)
-    contents
-      .should contain %(<div class="default">block content</div>)
-    {% if compare_versions(Crystal::VERSION, "0.36.1") > 0 %}
-      contents
-        .should contain %(<div @click="onclick($event)">block content</div>)
-    {% end %}
+    contents.should contain %(<div>text content</div>)
+    contents.should contain %(<div class="default">text content</div>)
+    contents.should contain %(<div class="default appended classes">text content</div>)
+    contents.should contain %(<div class="replaced">text content</div>)
+    contents.should contain %(<div id="foo" class="appended-without-default">text content</div>)
+    contents.should contain %(<div id="foo" class="replaced-without-default">text content</div>)
+    contents.should contain %(<div>block content</div>)
+    contents.should contain %(<div @click="onclick($event)">block content</div>)
   end
-end
-
-private def name_field
-  Avram::PermittedAttribute(String).new(
-    name: :name,
-    param: "",
-    value: "",
-    param_key: "user"
-  )
 end
