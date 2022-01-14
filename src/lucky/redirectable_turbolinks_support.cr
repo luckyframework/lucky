03f95cdd36f50fbe51e304f8587cd9b98bff1901
Rémy Marronnier <rmarronnier@gmail.com>
2022-01-14 23:56:22 +0100

    Removing obsolete links

--- src/lucky/redirectable_turbolinks_support.cr
+++ src/lucky/redirectable_turbolinks_support.cr
@@ -1,5 +1,5 @@
 # Set "Turbo-Location" from session
-# Needs to change browser address bar at last request, see https://github.com/turbo/turbo#following-redirects
+# Needs to change browser address bar at last request
 #
 # This pipe extracted Lucky::Redirectable, because Lucky::Redirectable included to Lucky::ErrorAction
 # but Lucky::ErrorAction not have pipe support
@@ -11,7 +11,7 @@ module Lucky::RedirectableTurboSupport
   private def set_turbo_location_header_from_session
     if turbo_location = cookies.get?(:_turbo_location)
       cookies.delete(:_turbo_location)
-      # change browser address bar at last request, see https://github.com/turbo/turbo#following-redirects
+      # change browser address bar at last request
       response.headers["Turbo-Location"] = turbo_location
     end
     continue
