6f68e0645fb9aad18f7658e1d6893499d2c24d8d
Jeremy Woertink <jeremywoertink@gmail.com>
2022-03-07 08:06:45 -0800

    reworks the message verifier to remove the magic -- character. (#1674)
    * reworks the message verifier to remove the magic -- character. This should stop random failures caused by duplicating the magic character. Fixes #1595
    
    * adding in a deprecated method to attemp decoding legacy tokens. This should help with the transition to new tokens.
    
    * swap checking for the new token before trying legacy token with the message verifier
    
    * adding specs for verify_raw and updating so it runs the new method first before falling back to legacy checks

--- src/lucky/support/message_verifier.cr
+++ src/lucky/support/message_verifier.cr
@@ -6,28 +6,58 @@ module Lucky
     def initialize(@secret : String, @digest = :sha1)
     end
 
-    def valid_message?(data, digest) : Bool
+    def valid_message?(data : String, digest : String) : Bool
       data.size > 0 && digest.size > 0 && Crypto::Subtle.constant_time_compare(digest, generate_digest(data))
     end
 
-    def verified(signed_message : String) : String?
+    @[Deprecated("Legacy token verification will be removed in the next release.")]
+    def legacy_verified(signed_message : String)
       data, digest = signed_message.split("--", 2)
-      if valid_message?(data, digest)
-        String.new(decode(data))
+      {data, digest}
+    rescue IndexError
+      {nil, nil}
+    end
+
+    def verified(signed_message : String) : String?
+      json_data = ::Base64.decode_string(signed_message)
+      data, digest = Tuple(String, String).from_json(json_data)
+
+      if valid_message?(data.to_s, digest.to_s)
+        String.new(decode(data.to_s))
+      end
+    rescue JSON::ParseException | Base64::Error
+      data, digest = legacy_verified(signed_message)
+
+      if (data && digest).nil?
+        return nil
+      end
+
+      if valid_message?(data.to_s, digest.to_s)
+        String.new(decode(data.to_s))
       end
     rescue argument_error : ArgumentError
       return if argument_error.message =~ %r{invalid base64}
       raise argument_error
     end
 
-    def verify(signed_message) : String
+    def verify(signed_message : String) : String
       verified(signed_message) || raise(InvalidSignatureError.new)
     end
 
     def verify_raw(signed_message : String) : Bytes
-      data, digest = signed_message.split("--", 2)
-      if valid_message?(data, digest)
-        decode(data)
+      begin
+        json_data = ::Base64.decode_string(signed_message)
+        data, digest = Tuple(String, String).from_json(json_data)
+      rescue JSON::ParseException | Base64::Error
+        data, digest = legacy_verified(signed_message)
+      end
+
+      if (data && digest).nil?
+        raise(InvalidSignatureError.new)
+      end
+
+      if valid_message?(data.to_s, digest.to_s)
+        decode(data.to_s)
       else
         raise(InvalidSignatureError.new)
       end
@@ -35,7 +65,7 @@ module Lucky
 
     def generate(value : String | Bytes) : String
       data = encode(value)
-      "#{data}--#{generate_digest(data)}"
+      encode({data, generate_digest(data)}.to_json)
     end
 
     private def encode(data) : String
