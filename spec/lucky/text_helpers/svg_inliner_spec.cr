6220cadc7d8d6ed57cfc8e957fcb782f838c7989
Wout <hi@wout.codes>
2022-11-15 21:05:38 +0100

    Add test for inlined svg with its original styling

--- spec/lucky/text_helpers/svg_inliner_spec.cr
+++ spec/lucky/text_helpers/svg_inliner_spec.cr
@@ -8,6 +8,10 @@ class TextHelperTestPage
   def with_inlined_svg
     inline_svg("lucky_logo.svg")
   end
+
+  def with_inlined_and_styled_svg
+    inline_svg("lucky_logo.svg", false)
+  end
 end
 
 describe Lucky::SvgInliner do
@@ -19,6 +23,15 @@ describe Lucky::SvgInliner do
       inlined_svg.should_not contain %(<!-- lucky logo -->)
       inlined_svg.should_not contain "\n"
       inlined_svg.should_not contain %(fill="none" stroke="#2a2a2a" class="logo")
+    end
+
+    it "inlines an svg in a page with its original styling attributes" do
+      inlined_svg = view.tap(&.with_inlined_and_styled_svg).render
+      inlined_svg.should start_with %(<svg data-inline-svg-styled="lucky_logo.svg")
+      inlined_svg.should contain %(fill="none" stroke="#2a2a2a" class="logo")
+      inlined_svg.should_not contain %(<?xml version="1.0" encoding="UTF-8"?>)
+      inlined_svg.should_not contain %(<!-- lucky logo -->)
+      inlined_svg.should_not contain "\n"
     end
   end
 end
