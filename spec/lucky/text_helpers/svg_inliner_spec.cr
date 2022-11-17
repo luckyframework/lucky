a50e31dc3dbffa398f5490b1052caafad34d48c6
Wout <hi@wout.codes>
2022-11-17 09:00:15 +0100

    Create separate test cases for stripping out unwanted elements in SVGs

--- spec/lucky/text_helpers/svg_inliner_spec.cr
+++ spec/lucky/text_helpers/svg_inliner_spec.cr
@@ -25,13 +25,30 @@ describe Lucky::SvgInliner do
       inlined_svg.should_not contain %(fill="none" stroke="#2a2a2a" class="logo")
     end
 
-    it "inlines an svg in a page with its original styling attributes" do
+    it "strips the xml declaration" do
+      inlined_svg = view.tap(&.with_inlined_svg).render
+      inlined_svg.should_not contain %(<?xml version="1.0" encoding="UTF-8"?>)
+    end
+
+    it "strips comments" do
+      inlined_svg = view.tap(&.with_inlined_svg).render
+      inlined_svg.should_not contain %(<!-- lucky logo -->)
+    end
+
+    it "strips newlines" do
+      inlined_svg = view.tap(&.with_inlined_svg).render
+      inlined_svg.should_not contain "\n"
+    end
+
+    it "strips styling attributes by default" do
+      inlined_svg = view.tap(&.with_inlined_svg).render
+      inlined_svg.should_not contain %(fill="none" stroke="#2a2a2a" class="logo")
+    end
+
+    it "allows inlinging an svg without stripping its styling attributes" do
       inlined_svg = view.tap(&.with_inlined_and_styled_svg).render
       inlined_svg.should start_with %(<svg data-inline-svg-styled="lucky_logo.svg")
       inlined_svg.should contain %(fill="none" stroke="#2a2a2a" class="logo")
-      inlined_svg.should_not contain %(<?xml version="1.0" encoding="UTF-8"?>)
-      inlined_svg.should_not contain %(<!-- lucky logo -->)
-      inlined_svg.should_not contain "\n"
     end
   end
 end
