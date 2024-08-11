81032529c18c8d632925880fc88195fad026282b
akadusei <attakusiadusei@gmail.com>
2024-08-11 14:52:13 +0000

    Fix invalid query string for array params

--- spec/lucky/base_http_client_spec.cr
+++ spec/lucky/base_http_client_spec.cr
@@ -3,6 +3,8 @@ require "../spec_helper"
 class HelloWorldAction < TestAction
   accepted_formats [:plain_text]
 
+  param codes : Array(String)?
+
   post "/hello" do
     plain_text "world"
   end
@@ -45,6 +47,13 @@ describe Lucky::BaseHTTPClient do
 
         request = TestServer.last_request
         request.body.to_s.should eq({foo: "bar"}.to_json)
+      end
+
+      it "works with array query params" do
+        response = MyClient.new.exec HelloWorldAction.with(codes: ["ab", "xy"])
+
+        request = TestServer.last_request
+        request.query.should eq("codes%5B%5D=ab&codes%5B%5D=xy")
       end
     end
 
