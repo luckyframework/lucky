a4bce2cfcc7cd1c36539d0d40c2591bbabc9ddc5
Russ Smith <russ@bashme.org>
2024-10-06 12:33:36 -0700

    Action based rate limiting.
    Include the module and write a rate_limit method to define the limit.
    The rate_limit_key method can be overridden if you want differeny key
    logic.
    
    Ref #1865

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -48,4 +48,8 @@ Lucky::ForceSSLHandler.configure do |settings|
   settings.enabled = true
 end
 
+LuckyCache.configure do |settings|
+  settings.storage = LuckyCache::MemoryStore.new
+end
+
 Habitat.raise_if_missing_settings!
