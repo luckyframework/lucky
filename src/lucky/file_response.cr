a05c064f3f442a25808ca4c121b6cea873e0a9dd
Agustin Arce <agustinarceprincich@gmail.com>
2024-10-11 22:02:04 -0300

    refactor (file_response): improve version check macro

--- src/lucky/file_response.cr
+++ src/lucky/file_response.cr
@@ -120,7 +120,7 @@ class Lucky::FileResponse < Lucky::Response
     File.expand_path(path, Dir.current)
   end
 
-  {% if Crystal::VERSION.split(".")[0].to_i > 1 || (Crystal::VERSION.split(".")[0].to_i == 1 && Crystal::VERSION.split(".")[1].to_i > 12) || (Crystal::VERSION.split(".")[0].to_i == 1 && Crystal::VERSION.split(".")[1].to_i == 12 && Crystal::VERSION.split(".")[2].to_i >= 0) %}
+  {% if compare_versions(Crystal::VERSION, "1.13.0") >= 0 %}
     private def file_exists? : Bool
       File.file?(full_path) && File::Info.readable?(full_path)
     end
