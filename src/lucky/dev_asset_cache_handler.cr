c9af0cb695b2a912f1ca96c1c578fe8b2c5132b4
Wout <hi@wout.codes>
2026-02-28 19:32:29 +0100

    Add asset cache handler for development

---
+++ src/lucky/dev_asset_cache_handler.cr
@@ -0,0 +1,34 @@
+class Lucky::DevAssetCacheHandler
+  include HTTP::Handler
+
+  ASSET_EXTENSIONS = %w[
+    .js
+    .css
+    .map
+    .json
+    .png
+    .jpg
+    .jpeg
+    .gif
+    .svg
+    .webp
+    .woff
+    .woff2
+    .ttf
+    .eot
+  ]
+
+  def call(context : HTTP::Server::Context) : Nil
+    if LuckyEnv.development? && asset_request?(context.request.path)
+      context.response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
+      context.response.headers["Pragma"] = "no-cache"
+      context.response.headers["Expires"] = "0"
+    end
+
+    call_next(context)
+  end
+
+  private def asset_request?(path : String) : Bool
+    ASSET_EXTENSIONS.any? { |ext| path.ends_with?(ext) }
+  end
+end
