5cf56a07f239af05c4e83e990b5123da0060082a
Wout <hi@wout.codes>
2026-03-01 11:36:49 +0100

    Add spec for enabled flag in dev asset cache handler

--- spec/lucky/dev_asset_cache_handler_spec.cr
+++ spec/lucky/dev_asset_cache_handler_spec.cr
@@ -6,7 +6,7 @@ describe Lucky::DevAssetCacheHandler do
   it "sets no-cache headers for static assets" do
     %w[/main.css /main.js /font.woff2 /image.png].each do |path|
       context = build_context(path: path)
-      handler = Lucky::DevAssetCacheHandler.new
+      handler = Lucky::DevAssetCacheHandler.new(enabled: true)
       handler.next = ->(_ctx : HTTP::Server::Context) { }
       handler.call(context)
 
@@ -19,12 +19,22 @@ describe Lucky::DevAssetCacheHandler do
   it "does not set no-cache headers for non-static assets" do
     %w[/some/page].each do |path|
       context = build_context(path: path)
-      handler = Lucky::DevAssetCacheHandler.new
+      handler = Lucky::DevAssetCacheHandler.new(enabled: true)
       handler.next = ->(_ctx : HTTP::Server::Context) { }
       handler.call(context)
 
       context.response.headers["Cache-Control"]?.should be_nil
       context.response.headers["Expires"]?.should be_nil
     end
+  end
+
+  it "does nothing if the handler is disabled" do
+    context = build_context(path: "/main.css")
+    handler = Lucky::DevAssetCacheHandler.new(enabled: false)
+    handler.next = ->(_ctx : HTTP::Server::Context) { }
+    handler.call(context)
+
+    context.response.headers["Cache-Control"]?.should be_nil
+    context.response.headers["Expires"]?.should be_nil
   end
 end
