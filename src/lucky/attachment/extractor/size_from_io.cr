e57392ff1c99efeda424f88b3e1370f51ae2ecc1
Wout <hi@wout.codes>
2026-03-18 08:14:56 +0100

    Get file size directly from IO in `SizeFromIO` extractor

--- src/lucky/attachment/extractor/size_from_io.cr
+++ src/lucky/attachment/extractor/size_from_io.cr
@@ -3,6 +3,6 @@ struct Lucky::Attachment::Extractor::SizeFromIO
 
   # Tries to extract the file size from the IO.
   def extract(uploaded_file, metadata, **options) : Int64?
-    uploaded_file.size.try &.to_i64
+    uploaded_file.tempfile.size
   end
 end
