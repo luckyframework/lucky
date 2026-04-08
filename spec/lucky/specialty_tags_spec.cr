d72fc3385eb0e64f088f320ddd4eae622a634b69
Wout <hi@wout.codes>
2026-04-08 20:10:23 +0200

    Make sure css is always reloaded (#2032)
    * Memoize Bun config so it's only loaded once
    
    * Make sure CSS is always fresh with a timestamp query param
    
    * Add tests for bun `css_link` and `bun_connect_reload_tag`
    
    * Test on given asset name to add timestamp rather than environment
    
    The environment is not a guarantee to add a timestamp or not. We match
    now on the asset path and whether or not the asset is fingerprinted.
    
    * Use the CSS file's mtime for cache busting

--- spec/lucky/specialty_tags_spec.cr
+++ spec/lucky/specialty_tags_spec.cr
@@ -26,6 +26,36 @@ describe Lucky::SpecialtyTags do
     HTML
   end
 
+  it "cache-busts non-fingerprinted local css links" do
+    html = view(&.css_link("/assets/css/app.css"))
+
+    html.should contain "bust="
+  end
+
+  it "does not cache-bust fingerprinted local css links" do
+    html = view(&.css_link("/assets/css/app-5e6f7a8b.css"))
+
+    html.should_not contain "bust="
+  end
+
+  it "does not cache-bust external css links" do
+    html = view(&.css_link("https://fonts.googleapis.com/css?family=Inter"))
+
+    html.should_not contain "bust="
+  end
+
+  it "does not cache-bust protocol-relative css links" do
+    html = view(&.css_link("//cdn.example.com/style.css"))
+
+    html.should_not contain "bust="
+  end
+
+  it "does not cache-bust css links outside the asset path" do
+    html = view(&.css_link("/other/path/style.css"))
+
+    html.should_not contain "bust="
+  end
+
   it "renders js link tag" do
     view(&.js_link("app.js")).should contain <<-HTML
     <script src="app.js"></script>
@@ -60,6 +90,29 @@ describe Lucky::SpecialtyTags do
     view(&.canonical_link("https://it.is/here")).should contain <<-HTML
     <link href="https://it.is/here" rel="canonical">
     HTML
+  end
+
+  it "renders bun reload script in development" do
+    html = view(&.bun_reload_connect_tag)
+
+    html.should contain "<script>"
+    html.should contain "new WebSocket"
+    html.should contain "ws://127.0.0.1:3002"
+  end
+
+  it "uses bust param for css hmr" do
+    html = view(&.bun_reload_connect_tag)
+
+    html.should contain "searchParams.set('bust'"
+  end
+
+  it "does not render bun reload script in production" do
+    ENV["LUCKY_ENV"] = "production"
+    html = view(&.bun_reload_connect_tag)
+
+    html.should_not contain "<script>"
+  ensure
+    ENV["LUCKY_ENV"] = "development"
   end
 
   it "renders proper non-breaking space entity" do
